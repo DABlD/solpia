6ad76a50dd53c24717b0ca16d8fa41a8507dcda9
David Mendoza <davidmendozaofficial@gmail.com>
2018-12-31 21:56:49 +0800

    Change toolbar color
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/users/includes/toolbar.blade.php
+++ resources/views/users/includes/toolbar.blade.php
@@ -1,5 +1,5 @@
 <div class="pull-right">
-	<a href="{{ route('users.index') }}" class="btn btn-default" data-toggle="tooltip" title="View All">
+	<a href="{{ route('users.index') }}" class="btn btn-info" data-toggle="tooltip" title="View All">
 		<span class="fa fa-list"></span>
 	</a>
 	<a href="{{ route('users.create') }}" class="btn btn-primary" data-toggle="tooltip" title="Add User">
