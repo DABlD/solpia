6c174b980d069fb57c1ad7beb39a800e2c94a817
David Mendoza <davidmendozaofficial@gmail.com>
2019-01-08 17:16:05 +0800

    Added Applicant Model
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,8 @@ class CreateUsersTable extends Migration
             $table->text('address');
             $table->string('contact');
 
+            $table->boolean('applicant')->default(0);
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2019_01_08_085921_create_applicants_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+
+            $table->string('birth_place');
+            $table->string('religion');
+
+            $table->text('provincial_address');
+            $table->string('provincial_contact');
+
+            $table->integer('age');
+            $table->double('height', 5, 2);
+            $table->double('weight', 5, 2);
+            $table->double('bmi', 5, 2);
+
+            $table->string('blood_type', 5);
+            $table->enum('civil_status', ['Single', 'Married', 'Widowed', 'Divorced']);
+
+            $table->string('tin')->nullable();
+            $table->string('sss')->nullable();
+
+            $table->double('waistline', 5, 2);
+            $table->double('shoes', 5, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicants');
+    }
+}
