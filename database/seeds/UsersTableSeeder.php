6e7579fdc0b78dce7ac1bb465e60333f03ade7c5
David Mendoza <davidmendozaofficial@gmail.com>
2019-01-08 22:38:26 +0800

    Added Applicant Role and Added New Applicant
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Models\{Applicant, EducationalBackground, FamilyData};
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,7 +14,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-        	'fname' => 'Admin',
+            'fname' => 'Admin',
             'mname' => 'admin',
             'lname' => 'Istrator',
             'role' => 'Admin',
@@ -22,8 +23,98 @@ class UsersTableSeeder extends Seeder
             'gender' => 'Male',
             'address' => 'Manila, Phillipines',
             'contact' => '09123456789',
+            'applicant' => false,
             'email_verified_at' => now()->toDateTimeString(),
             'password' => '123456'
+        ]);
+
+        User::create([
+        	'fname' => 'App',
+            'mname' => 'Lic',
+            'lname' => 'Cant',
+            'role' => 'Applicant',
+            'email' => 'applicant@applicant.com',
+            'birthday' => '1997-11-12',
+            'gender' => 'Male',
+            'address' => 'Manila, Phillipines',
+            'contact' => '09789456132',
+            'applicant' => true,
+            'email_verified_at' => now()->toDateTimeString(),
+            'password' => '123456'
+        ]);
+
+        Applicant::create([
+            'user_id' => 2,
+            'birth_place' => 'Somewhere in Phillipines',
+            'religion' => 'Catholic',
+            'provincial_address' => 'Manila, Phillipines',
+            'provincial_contact' => '09123456789',
+            'age' => 20,
+            'height' => 5.5,
+            'weight' => 60,
+            'bmi' => 23,
+            'blood_type' => 'O',
+            'civil_status' => 'Single',
+            'tin' => 17238172937,
+            'sss' => 128736182736,
+            'waistline' => 5.9,
+            'shoes' => 8,
+        ]);
+
+        EducationalBackground::create([
+            'applicant_id' => 1,
+            'type' => 'Elementary',
+            'course' => null,
+            'year' => '2003-2009',
+            'school' => 'Some Elementary School',
+            'address' => 'Somewhere in Phillipines',
+        ]);
+
+        EducationalBackground::create([
+            'applicant_id' => 1,
+            'type' => 'High School',
+            'course' => null,
+            'year' => '2009-2013',
+            'school' => 'Some High School',
+            'address' => 'Somewhere in Phillipines',
+        ]);
+
+        EducationalBackground::create([
+            'applicant_id' => 1,
+            'type' => 'College',
+            'course' => 'BSIT',
+            'year' => '2013-2019',
+            'school' => 'FEU Institute of Technology',
+            'address' => 'Sampaloc, Manila',
+        ]);
+
+        FamilyData::create([
+            'applicant_id' => 1,
+            'type' => 'Father',
+            'name' => 'Fa Ther Cant',
+            'age' =>  40,
+            'birthday' => '1997-11-12', 
+            'address' => 'Manila, Phillipines',
+            'occupation' => 'Seaman',
+        ]);
+
+        FamilyData::create([
+            'applicant_id' => 1,
+            'type' => 'Mother',
+            'name' => 'Mo Ther Cant',
+            'age' =>  38,
+            'birthday' => '1997-11-12', 
+            'address' => 'Manila, Phillipines',
+            'occupation' => 'Housewife',
+        ]);
+
+        FamilyData::create([
+            'applicant_id' => 1,
+            'type' => 'Brother',
+            'name' => 'Bro Ther Cant',
+            'age' =>  22,
+            'birthday' => '1997-11-12', 
+            'address' => 'Manila, Phillipines',
         ]);
     }
 }
