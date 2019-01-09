0e3f26780b7073a788e36cd212704ba2ba1a3b62
David Mendoza <davidmendozaofficial@gmail.com>
2019-01-09 18:33:05 +0800

    Changed defaulted applicant value
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ class CreateUsersTable extends Migration
             $table->text('address');
             $table->string('contact');
 
-            $table->boolean('applicant')->default(0);
+            $table->boolean('applicant')->default(1);
 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
