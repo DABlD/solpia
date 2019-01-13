3f777d6e1250e8f20288ed2a4f648694179f6ee1
David Mendoza <davidmendozaofficial@gmail.com>
2019-01-13 15:10:37 +0800

    Finished Exporting all Personal Data
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/Application.php
+++ app/Exports/Application.php
@@ -52,40 +52,54 @@ class Application implements FromView, ShouldAutoSize, WithEvents, WithDrawings
             ],
         ];
 
+        $headingStyle = [
+            'font' => [
+                'bold' => true
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            ]
+        ];
+
         return [
-            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle) {
+            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle, $headingStyle) {
                 $event->sheet->getDelegate()->getStyle('A1')->getFont()->setSize(18);
                 $event->sheet->getDelegate()->getStyle('A2')->getFont()->setSize(12);
 
-                $event->sheet->getDelegate()->getStyle('A1:A2')->applyFromArray([
-                    'font' => [
-                        'bold' => true
-                    ],
-                    'alignment' => [
-                        'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
-                    ]
-                ]);
+                $event->sheet->getDelegate()->getStyle('A1:A2')->applyFromArray($headingStyle);
+                $event->sheet->getDelegate()->getStyle('A9:N9')->applyFromArray($headingStyle);
 
                 // FILLS
-                $event->sheet->getDelegate()->getStyle('A3:A8')->applyFromArray($fillStyle);
+                $event->sheet->getDelegate()->getStyle('A3:A12')->applyFromArray($fillStyle);
                 $event->sheet->getDelegate()->getStyle('E3:E5')->applyFromArray($fillStyle);
                 $event->sheet->getDelegate()->getStyle('J3:J5')->applyFromArray($fillStyle);
                 $event->sheet->getDelegate()->getStyle('J3:J5')->applyFromArray($fillStyle);
                 $event->sheet->getDelegate()->getStyle('G8:L8')->applyFromArray($fillStyle);
+                $event->sheet->getDelegate()->getStyle('F10:F12')->applyFromArray($fillStyle);
+                $event->sheet->getDelegate()->getStyle('I10:I11')->applyFromArray($fillStyle);
+                $event->sheet->getDelegate()->getStyle('M10:M11')->applyFromArray($fillStyle);
+                $event->sheet->getDelegate()->getStyle('I12:N12')->applyFromArray($fillStyle);
 
                 $cells = [
                     'A1:N1','A2:N2','A3:B3','C3:D3','A4:B4',
                     'C4:D4','A5:B5','C5:D5','E3:F3','G3:I3',
                     'E4:F4','G4:I4','E5:F5','G5:I5','J3',
-                    'K3:L3','K4','L4','G4:I4','J5','K5','L5',
-                    'A6:C6', 'D6:L6', 'A7:C7', 'D7:L7',
-                    'A8:C8', 'D8:F8', 'G8:L8', 'G8:L8', 'M3:N8',
+                    'K3','L3','K4','L4','G4:I4','J5','K5','L5',
+                    'A6:C6','D6:L6','A7:C7','D7:L7',
+                    'A8:C8','D8:F8','G8:L8','G8:L8','M3:N8',
+                    'A9:N9',
+                    'A10:B10','C10:E10','F10:G10','H10','I10','J10:L10','M10','N10',
+                    'A11:B11','C11:E11','F11:G11','H11','I11','J11:L11','M11','N11',
+                    'A12:B12','C12:E12','F12:G12','H12','I12:N12'
                 ];
 
                 // BORDERS
                 foreach($cells as $cell){
                     $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($borderStyle);
                 }
+
+                $event->sheet->getDelegate()->getColumnDimension('N')->setAutoSize(false);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(6.6);
             },
         ];
     }
--- resources/views/exports/application.blade.php
+++ resources/views/exports/application.blade.php
@@ -19,10 +19,11 @@
 		<tr>
 			<td colspan="2">FIRST NAME</td>
 			<td colspan="2">{{ $applicant->user->fname }}</td>
-			<td colspan="2">BIRTHDAY</td>
-			<td colspan="3">{{ $applicant->user->birthday->format('F j, Y') }}</td>
-			<td colspan="1">GENDER</td>
-			<td colspan="2">{{ $applicant->user->gender }}</td>
+			<td colspan="2">AGE</td>
+			<td colspan="3">{{ $applicant->age }}</td>
+			<td colspan="1">HEIGHT</td>
+			<td colspan="1">{{ $applicant->height }}</td>
+			<td colspan="1">CM</td>
 			<td colspan="2" rowspan="6"></td>
 		</tr>
 
@@ -31,9 +32,9 @@
 			<td colspan="2">{{ $applicant->user->mname }}</td>
 			<td colspan="2">EMAIL</td>
 			<td colspan="3">{{ $applicant->user->email }}</td>
-			<td colspan="1">HEIGHT</td>
-			<td colspan="1">{{ $applicant->height }}</td>
-			<td colspan="1">CM</td>
+			<td colspan="1">WEIGHT</td>
+			<td colspan="1">{{ $applicant->weight }}</td>
+			<td colspan="1">KG</td>
 		</tr>
 
 		<tr>
@@ -41,9 +42,8 @@
 			<td colspan="2">{{ $applicant->user->lname }}</td>
 			<td colspan="2">CONTACT</td>
 			<td colspan="3">{{ $applicant->user->contact }}</td>
-			<td colspan="1">WEIGHT</td>
-			<td colspan="1">{{ $applicant->weight }}</td>
-			<td colspan="1">KG</td>
+			<td colspan="1">BMI</td>
+			<td colspan="2">{{ $applicant->bmi }}</td>
 		</tr>
 
 		<tr>
@@ -59,6 +59,40 @@
 		<tr>
 			<td colspan="3">PROVINCIAL CONTACT NO.</td>
 			<td colspan="3">{{ $applicant->provincial_contact }}</td>
+			<td colspan="6"></td>
+		</tr>
+
+		<tr>
+			<td colspan="14">OTHER PERSONAL DATA</td>
+		</tr>
+
+		<tr>
+			<td colspan="2">BIRTHDAY</td>
+			<td colspan="3">{{ $applicant->user->birthday->format('F j, Y') }}</td>
+			<td colspan="2">GENDER</td>
+			<td colspan="1">{{ $applicant->user->gender }}</td>
+			<td colspan="1">TIN</td>
+			<td colspan="3">{{ $applicant->tin }}</td>
+			<td colspan="1">SHOE SIZE</td>
+			<td colspan="1">{{ $applicant->shoe_size }}</td>
+		</tr>
+
+		<tr>
+			<td colspan="2">BIRTHPLACE</td>
+			<td colspan="3">{{ $applicant->birth_place }}</td>
+			<td colspan="2">BLOOD TYPE</td>
+			<td colspan="1">{{ $applicant->blood_type }}</td>
+			<td colspan="1">SSS</td>
+			<td colspan="3">{{ $applicant->sss }}</td>
+			<td colspan="1">WAISTLINE</td>
+			<td colspan="1">{{ $applicant->waistline }}</td>
+		</tr>
+
+		<tr>
+			<td colspan="2">RELIGION</td>
+			<td colspan="3">{{ $applicant->religion }}</td>
+			<td colspan="2">CIVIL STATUS</td>
+			<td colspan="1">{{ $applicant->civil_status }}</td>
 			<td colspan="6"></td>
 		</tr>
 
