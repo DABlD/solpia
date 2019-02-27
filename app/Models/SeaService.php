cd29f90866a32edde65fb540e444fd9285fa6fd9
David Mendoza <davidmendozaofficial@gmail.com>
2019-02-28 00:03:38 +0800

    Update Migration
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/SeaService.php
+++ app/Models/SeaService.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SeaService extends Model
 {
+	protected $fillable = [
+		
+	];
+
     protected $dates = [
         'sign_on', 'sign_off', 'created_at', 'updated_at'
     ];
--- database/migrations/2019_02_27_123642_create_sea_services_table.php
+++ database/migrations/2019_02_27_123642_create_sea_services_table.php
@@ -15,24 +15,24 @@ class CreateSeaServicesTable extends Migration
     {
         Schema::create('sea_services', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('vessel_name', 100);
-            $table->string('rank', 50);
-            $table->string('vessel_type', 50);
-            $table->string('gross_tonnage', 100);
-            $table->string('engine_type', 100);
-            $table->string('bhp_kw', 100);
-            $table->string('flag', 50);
-            $table->string('trade', 50);
-            $table->double('previous_salary', 8,2);
-            $table->string('manning_agent', 100);
-            $table->string('principal', 100);
-            $table->string('crew_nationality', 50);
-            $table->date('sign_on');
-            $table->date('sign_off');
-            $table->integer('total_months');
-            $table->text('remarks');
-            $table->string('charterer', 100);
-            $table->text('cargoes');
+            $table->string('vessel_name', 100)->nullable();
+            $table->string('rank', 50)->nullable();
+            $table->string('vessel_type', 50)->nullable();
+            $table->string('gross_tonnage', 100)->nullable();
+            $table->string('engine_type', 100)->nullable();
+            $table->string('bhp_kw', 100)->nullable();
+            $table->string('flag', 50)->nullable();
+            $table->string('trade', 50)->nullable();
+            $table->double('previous_salary', 8,2)->nullable();
+            $table->string('manning_agent', 100)->nullable();
+            $table->string('principal', 100)->nullable();
+            $table->string('crew_nationality', 50)->nullable();
+            $table->date('sign_on')->nullable();
+            $table->date('sign_off')->nullable();
+            $table->integer('total_months')->nullable();
+            $table->text('remarks')->nullable();
+            $table->string('charterer', 100)->nullable();
+            $table->text('cargoes')->nullable();
             $table->timestamps();
         });
     }
