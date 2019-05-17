f2d092dc05cac3932e541bad9b65a99c671b713b
David Mendoza <davidmendozaofficial@gmail.com>
2019-05-17 08:03:34 +0800

    Progrerss Shinko 2nd Worksheet
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ app/Exports/ShinkoDC.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\{Applicant};
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class ShinkoDC implements FromView, WithEvents//, WithDrawings//, ShouldAutoSize
+{
+    public function __construct($applicant,$type){
+        $this->applicant = $applicant;
+        $this->type = $type;
+    }
+
+    public function view(): View
+    {
+    	return view('exports.' . $this->type, [
+            'applicant' => $this->applicant
+        ]);
+    }
+
+    public function registerEvents(): array
+    {
+        $borderStyle = [
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'bottom' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'left' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'right' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ];
+
+        $fillStyle = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => [
+                    'rgb' => 'ced4da'
+                ]
+            ],
+        ];
+
+        $headingStyle = [
+            [
+            	'font' => [
+            	    'bold' => true
+            	],
+            	'alignment' => [
+            	    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            	]
+            ],
+            [
+            	'alignment' => [
+            	    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP,
+            	]
+            ],
+            [
+            	'font' => [
+            	    'bold' => true
+            	],
+            	'alignment' => [
+            	    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+            	]
+            ],
+            [
+            	'alignment' => [
+            	    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            	]
+            ],
+        ];
+
+        return [
+            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle, $headingStyle) {
+                // SHEET SETTINGS
+                $size = \PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4;
+                $event->sheet->getDelegate()->getPageSetup()->setPaperSize($size);
+                $event->sheet->getDelegate()->setTitle('DOCUMENT CHECKLIST', false);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(0);
+                $event->sheet->getDelegate()->getPageMargins()->setTop(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setBottom(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setRight(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setHeader(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setFooter(0.5);
+
+                // FONT SIZES
+
+                $event->sheet->getDelegate()->getStyle('A4:N60')->getFont()->setSize(11);
+                $event->sheet->getDelegate()->getStyle('A1')->getFont()->setSize(18);
+                $event->sheet->getDelegate()->getStyle('A2')->getFont()->setSize(12);
+
+                // ROWS
+                $rows = [
+                    'A1:N1', 'A2:N2'
+                ];
+
+                // FUNCTIONS
+
+                // HEADINGS
+
+                // HC B
+                $h[0] = [
+                	'A1:N1'
+                ];
+
+                // VT
+                $h[1] = [
+                	
+                ];
+
+                // HL B
+                $h[2] = [
+                	
+                ];
+
+                // HC
+                $h[3] = [
+                	'A6:N6'
+                ];
+
+                $h['wrap'] = [
+                	
+                ];
+
+                // $event->sheet->getDelegate()->getStyle('A1:N60')->getAlignment()->setWrapText(true);
+                foreach($h as $key => $value) {
+            		foreach($value as $col){
+            			if($key === 'wrap'){
+            				$event->sheet->getDelegate()->getStyle($col)->getAlignment()->setWrapText(true);
+            			}
+            			else{
+	            			$event->sheet->getDelegate()->getStyle($col)->applyFromArray($headingStyle[$key]);
+            			}
+            		}
+                }
+
+                // FILLS
+                $fills = [
+                    'A1', 'A3:A6', 'F3:F6', 'H6', 'J3:J6', 'L6'
+                ];
+
+                foreach($fills as $fill){
+                	$event->sheet->getDelegate()->getStyle($fill)->applyFromArray($fillStyle);	
+                }
+
+                // BORDERS
+                $cells = [
+                    'A1:N1',
+                    'A3:B3', 'C3:E3', 'F3:G3', 'H3:I3', 'J3:K3', 'L3:N3',
+                    'A4:B4', 'C4:E4', 'F4:G4', 'H4:I4', 'J4:K4', 'L4:N4',
+                    'A5:B5', 'C5:E5', 'F5:G5', 'H5:I5', 'J5:K5', 'L5:N5',
+                    'A6:E6', 'F6:G6', 'H6:I6', 'J6:K6', 'L6:N6',
+                ];
+
+                foreach($cells as $cell){
+                    $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($borderStyle);
+                }
+
+                // COLUMN RESIZE
+
+                // $event->sheet->getDelegate()->getColumnDimension('E')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('H')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('F')->setAutoSize(true);
+                // $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(10);
+                // $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(4);
+                $event->sheet->getDelegate()->getRowDimension('1')->setRowHeight(25);
+            },
+        ];
+    }
+}
---
+++ resources/views/exports/shinkoDC.blade.php
@@ -0,0 +1,90 @@
+<table>
+	<tbody>
+		<!-- HEADER -->
+		<tr>
+			<td colspan="14">
+				DOCUMENT CHECK LIST
+			</td>
+		</tr>
+		<tr>
+			<td colspan="14"></td>
+		</tr>
+
+		<!-- MAIN -->
+
+		<!-- 1st Row -->
+		<tr>
+			<td colspan="2">
+				VESSEL NAME
+			</td>
+			<td colspan="3"></td>
+
+			<td colspan="2">
+				FLAG
+			</td>
+			<td colspan="2"></td>
+
+			<td colspan="2">
+				TYPE
+			</td>
+			<td colspan="3"></td>
+		</tr>
+
+		<!-- 2nd Row -->
+		<tr>
+			<td colspan="2">
+				SEAMAN'S NAME
+			</td>
+			<td colspan="3"></td>
+
+			<td colspan="2">
+				RANK
+			</td>
+			<td colspan="2"></td>
+
+			<td colspan="2">
+				NATIONALITY
+			</td>
+			<td colspan="3"></td>
+		</tr>
+
+		<!-- 3rd Row -->
+		<tr>
+			<td colspan="2">
+				BIRTH DATE
+			</td>
+			<td colspan="3"></td>
+
+			<td colspan="2">
+				JOINING DATE
+			</td>
+			<td colspan="2"></td>
+
+			<td colspan="2">
+				SIGN OF VERIFIER
+			</td>
+			<td colspan="3"></td>
+		</tr>
+
+		<!-- 4th Row -->
+		<tr>
+			<td colspan="5">
+				DOCUMENTS
+			</td>
+
+			<td colspan="2">
+				NUMBER
+			</td>
+			<td colspan="2">
+				ISSUE
+			</td>
+
+			<td colspan="2">
+				EXPIRY
+			</td>
+			<td colspan="3">
+				REMARK
+			</td>
+		</tr>
+	</tbody>
+</table>
