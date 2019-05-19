84d3a8133d1b4df3e3303107dc9e3205e25e02f7
David Mendoza <davidmendozaofficial@gmail.com>
2019-05-20 00:34:25 +0800

    Shinko LC WIP
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ app/Exports/ShinkoLC.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\{Applicant};
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class ShinkoLC implements FromView, WithEvents//, WithDrawings//, ShouldAutoSize
+{
+    public function __construct($applicant,$type){
+        $this->applicant = $applicant;
+        $this->type = $type;
+    }
+
+    public function view(): View
+    {
+        return view('exports.' . $this->type, [
+            'applicant' => $this->applicant
+        ]);
+    }
+
+    public function registerEvents(): array
+    {
+        $borderStyle = [
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'bottom' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'left' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'right' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ];
+
+        $fillStyle = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => [
+                    'rgb' => 'ced4da'
+                ]
+            ],
+        ];
+
+        $headingStyle = [
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP,
+                ]
+            ],
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+        ];
+
+        return [
+            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle, $headingStyle) {
+                // SHEET SETTINGS
+                $size = \PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4;
+                $event->sheet->getDelegate()->getPageSetup()->setPaperSize($size);
+                $event->sheet->getDelegate()->setTitle('INTERVIEW LIST', false);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(0);
+                $event->sheet->getDelegate()->getPageMargins()->setTop(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setBottom(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setRight(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setHeader(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setFooter(0.5);
+
+                // FONT SIZES
+
+                $event->sheet->getDelegate()->getStyle('A4:N60')->getFont()->setSize(11);
+                $event->sheet->getDelegate()->getStyle('A1')->getFont()->setSize(18);
+                $event->sheet->getDelegate()->getStyle('A2')->getFont()->setSize(12);
+
+                // FAMILY DATA ROWS
+
+                // FUNCTIONS
+
+                // HEADINGS
+
+                // HC B
+                $h[0] = [
+                    'A1:A2'
+                ];
+
+                // VT
+                $h[1] = [
+                    
+                ];
+
+                // HL B
+                $h[2] = [
+                    
+                ];
+
+                // HC
+                $h[3] = [
+                    
+                ];
+
+                // HL
+                $h[4] = [
+                    
+                ];
+
+                $h['wrap'] = [
+                    
+                ];
+
+                // $event->sheet->getDelegate()->getStyle('A1:N60')->getAlignment()->setWrapText(true);
+                foreach($h as $key => $value) {
+                    foreach($value as $col){
+                        if($key === 'wrap'){
+                            $event->sheet->getDelegate()->getStyle($col)->getAlignment()->setWrapText(true);
+                        }
+                        else{
+                            $event->sheet->getDelegate()->getStyle($col)->applyFromArray($headingStyle[$key]);
+                        }
+                    }
+                }
+
+                // FILLS
+                $fills = [
+
+                ];
+
+                foreach($fills as $fill){
+                    $event->sheet->getDelegate()->getStyle($fill)->applyFromArray($fillStyle);  
+                }
+
+                // BORDERS
+                $cells = [
+                    
+                ];
+
+                foreach($cells as $cell){
+                    $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($borderStyle);
+                }
+
+                // COLUMN RESIZE
+
+                // $event->sheet->getDelegate()->getColumnDimension('E')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('H')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('F')->setAutoSize(true);
+                $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(4);
+            },
+        ];
+    }
+}
---
+++ resources/views/exports/shinkoLC.blade.php
@@ -0,0 +1,38 @@
+
+<table>
+	<tbody>
+		<tr>
+			<td colspan="14">
+				SHINKO MARITIME CO., LTD.
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="14">
+				APPLICATION FOR EMPLOYMENT
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="3">
+				FOR FOREIGN
+			</td>
+		</tr>
+
+		<tr>
+			<td>*</td>
+		</tr>
+
+		<tr>
+			<td>Please complete only bolded</td>
+		</tr>
+
+		<tr></tr>
+		<tr>
+			<td colspan="2">Ship's Name</td>
+			<td colspan="3"></td>
+			<td colspan="3">Rank</td>
+			<td colspan="4"></td>
+		</tr>
+	</tbody>
+</table>
