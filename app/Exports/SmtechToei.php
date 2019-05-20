f9416607aa2d0e1f5e368ca3e358f93b9f89d40a
David Mendoza <davidmendozaofficial@gmail.com>
2019-05-21 04:58:04 +0800

    SmtechToei WIP 1st Part
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/SmtechToei.php
+++ app/Exports/SmtechToei.php
@@ -2,15 +2,222 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use App\Models\{Applicant};
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithDrawings;
 
-class SmtechToei implements FromCollection
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class SmtechToei implements FromView, WithEvents//, WithDrawings//, ShouldAutoSize
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    public function __construct($applicant,$type){
+        $this->applicant = $applicant;
+        $this->type = $type;
+    }
+
+    public function view(): View
     {
-        //
+        return view('exports.' . $this->type, [
+            'applicant' => $this->applicant
+        ]);
+    }
+
+    public function registerEvents(): array
+    {
+        $borderStyle = [
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'bottom' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'left' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+                'right' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ];
+
+        $fillStyle = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => [
+                    'rgb' => 'ced4da'
+                ]
+            ],
+        ];
+
+        $headingStyle = [
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP,
+                ]
+            ],
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+                ],
+            ],
+            [
+                'font' => [
+                    'bold' => true
+                ],
+            ]
+        ];
+
+        return [
+            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle, $headingStyle) {
+                // SHEET SETTINGS
+                $size = \PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4;
+                $event->sheet->getDelegate()->getPageSetup()->setPaperSize($size);
+                $event->sheet->getDelegate()->setTitle('BIODATA', false);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(0);
+                $event->sheet->getDelegate()->getPageMargins()->setTop(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setBottom(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setRight(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setHeader(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setFooter(0.5);
+
+                $event->sheet->getParent()->getDefaultStyle()->getFont()->setName('Arial');
+                $event->sheet->getParent()->getDefaultStyle()->getFont()->setSize(10);
+                // $event->sheet->getDelegate()->getDefaultStyle()->getFont()->setName('Arial');
+                // $event->sheet->getDelegate()->getDefaultStyle()->getFont()->setSize(10);
+
+                // FONT SIZES
+
+                // FAMILY DATA ROWS
+                $rows = [
+
+                ];
+
+                // FUNCTIONS
+                
+                // HEADINGS
+
+                // HC B
+                $h[0] = [
+                    
+                ];
+
+                // VT
+                $h[1] = [
+                    
+                ];
+
+                // HL B
+                $h[2] = [
+                    
+                ];
+
+                // HC
+                $h[3] = [
+                    
+                ];
+
+                // HL
+                $h[4] = [
+                    
+                ];
+
+                // HC VC
+                $h[5] = [
+                    
+                ];
+
+                // B
+                $h[6] = [
+                    
+                ];
+
+                $h['wrap'] = [
+                    
+                ];
+
+                // $event->sheet->getDelegate()->getStyle('A1:N60')->getAlignment()->setWrapText(true);
+                foreach($h as $key => $value) {
+                    foreach($value as $col){
+                        if($key === 'wrap'){
+                            $event->sheet->getDelegate()->getStyle($col)->getAlignment()->setWrapText(true);
+                        }
+                        else{
+                            $event->sheet->getDelegate()->getStyle($col)->applyFromArray($headingStyle[$key]);
+                        }
+                    }
+                }
+
+                // FILLS
+                $fills = [
+                    
+                ];
+
+                foreach($fills as $fill){
+                    $event->sheet->getDelegate()->getStyle($fill)->applyFromArray($fillStyle);  
+                }
+
+                // BORDERS
+                $cells = array_merge($rows, [
+                      
+                ]);
+
+                foreach($cells as $cell){
+                    $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($borderStyle);
+                }
+
+                // COLUMN RESIZE
+
+                // $event->sheet->getDelegate()->getColumnDimension('E')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('H')->setAutoSize(false);
+                // $event->sheet->getDelegate()->getColumnDimension('F')->setAutoSize(true);
+                // $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(10);
+                // $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(4);
+            },
+        ];
+    }
+
+    public function drawings()
+    {
+        $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+		$drawing->setName('Logo');
+		$drawing->setDescription('Logo');
+        $drawing->setPath(public_path($this->applicant->user->avatar));
+        $drawing->setHeight(154);
+        $drawing->setOffsetX(22);
+		$drawing->setCoordinates('L3');
+
+        return $drawing;
     }
 }
---
+++ resources/views/exports/smtechToei.blade.php
@@ -0,0 +1,97 @@
+<table>
+	<tbody>
+		<tr>
+			<td colspan="7"></td>
+			<td colspan="2">CHECKED BY</td>
+		</tr>
+
+		<tr>
+			<td colspan="2" rowspan="8"></td>
+			<td></td>
+			<td colspan="3">BIO-DATA</td>
+			<td></td>
+			<td></td>
+			<td>INCHARGE</td>
+		</tr>
+
+		<tr>
+			<td colspan="5"></td>
+			<td rowspan="3"></td>
+			<td rowspan="3"></td>
+		</tr>
+
+		<tr>
+			<td colspan="5"></td>
+		</tr>
+
+		<tr>
+			<td colspan="5"></td>
+		</tr>
+
+		<tr>
+			<td colspan="7"></td>
+		</tr>
+
+		<tr>
+			<td colspan="2">Manning Agent:</td>
+			<td colspan="2"></td>
+			<td>Presenter:</td>
+			<td colspan="2">SOLPIA</td>
+		</tr>
+
+		<tr>
+			<td colspan="7"></td>
+		</tr>
+
+		<tr>
+			<td colspan="4"></td>
+			<td>Date:</td>
+			<td colspan="2"></td>
+		</tr>
+
+		<tr>
+			<td colspan="9"></td>
+		</tr>
+
+		<tr>
+			<td>Code No.:</td>
+			<td></td>
+			<td>Rank:</td>
+			<td></td>
+			<td>Date Employed:</td>
+			<td></td>
+			<td>Vessel:</td>
+			<td colspan="2"></td>
+		</tr>
+
+		<tr>
+			<td>Name</td>
+			<td colspan="2"></td>
+			<td colspan="2"></td>
+			<td colspan="2"></td>
+			<td colspan="2"></td>
+		</tr>
+
+		<tr>
+			<td></td>
+			<td colspan="2">(Surname)</td>
+			<td colspan="2">(Given Name)</td>
+			<td colspan="2">(Middle Name)</td>
+			<td colspan="2">(Chinese Character)</td>
+		</tr>
+
+		<tr>
+			<td>Address:</td>
+			<td colspan="5"></td>
+			<td></td>
+			<td colspan="2"></td>
+		</tr>
+
+		<tr>
+			<td></td>
+			<td colspan="5"></td>
+			<td>Email:</td>
+			<td colspan="2"></td>
+		</tr>
+	</tbody>
+</table>
