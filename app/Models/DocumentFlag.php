2a1b312159dcc58d6e3195186e422b1aeb2812c9
David Mendoza <davidmendozaofficial@gmail.com>
2019-06-05 10:59:57 +0800

    Added issue_date and expiry_date
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/DocumentFlag.php
+++ app/Models/DocumentFlag.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentFlag extends Model
 {
-    protected $fillable = ['country', 'booklet_no', 'license_no', 'goc', 'sso', 'sdsd', 'applicant_id'];
+    protected $fillable = [
+    	'country', 'booklet_no', 'license_no', 'goc', 'sso', 'sdsd', 'applicant_id',
+    	'issue_date', 'expiry_date'
+    ];
 
     protected $dates = [
-        'created_at', 'updated_at', 'deleted_at'
+        'created_at', 'updated_at', 'deleted_at', 'issue_date', 'expiry_date'
     ];
 }
