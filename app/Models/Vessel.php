c28b1686e6a6301c5fa7a4d6cc811dc6ea4264c6
David Mendoza <davidmendozaofficial@gmail.com>
2019-06-07 10:13:07 +0800

    Defined dates, fillable and relationship
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/Vessel.php
+++ app/Models/Vessel.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vessel extends Model
 {
-    //
+    protected $fillable = [
+    	'principal_id', 'name', 'flag', 'type', 'year_build', 'builder', 'engine', 'gross_tonnage', 'BHP', 'trade', 'ecdis', 'status'
+    ];
+
+    protected $dates = [
+    	'created_at', 'updated_at'
+    ];
+
+    public function principal(){
+    	return $this->belongsTo('App\Principal');
+    }
 }
