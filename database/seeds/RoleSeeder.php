0ab159aae9b5b1817b85ff37caa9f5d213050288
David Mendoza <davidmendozaofficial@gmail.com>
2019-06-07 10:35:09 +0800

    Added Principal Role
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -15,5 +15,6 @@ class RoleSeeder extends Seeder
         Role::create(['name' => 'Admin']);
         Role::create(['name' => 'Encoder']);
         Role::create(['name' => 'Applicant']);
+        Role::create(['name' => 'Principal']);
     }
 }
