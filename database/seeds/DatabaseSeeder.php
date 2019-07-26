84557cca729a889e03dacb41d5ab7c58984927ec
David Mendoza <davidmendozaofficial@gmail.com>
2019-07-26 15:29:46 +0800

    Removed VesselSeeder
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(VesselSeeder::class);
+        // $this->call(VesselSeeder::class);
         $this->call(RankSeeder::class);
     }
 }
