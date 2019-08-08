4d3a13c3ab2ee563611aaf1c0d7f7a31fc0163d7
David Mendoza <davidmendozaofficial@gmail.com>
2019-08-08 13:42:04 +0800

    Added softdeletes in eb, ss, fd
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/FamilyData.php
+++ app/Models/FamilyData.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FamilyData extends Model
 {
+	use SoftDeletes;
+	
     protected $fillable = [
     	'applicant_id','type','name',
     	'age','birthday','address',
--- app/Models/SeaService.php
+++ app/Models/SeaService.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SeaService extends Model
 {
+	use SoftDeletes;
+	
 	protected $fillable = [
 		'vessel_name','rank','vessel_type','gross_tonnage','engine_type','bhp_kw','flag','trade','previous_salary','manning_agent','principal','crew_nationality','sign_on','sign_off','total_months','remarks', 'applicant_id',
 		'smc'
