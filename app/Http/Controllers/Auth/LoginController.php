95be07ae60150b73823d29845dd6daed4efbd03c
David Mendoza <davidmendozaofficial@gmail.com>
2019-08-15 11:55:53 +0800

    Can login with username or email
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
     }
 
     // OVERRIDEN FUNCTIONS FROM AuthenticatesUsers
-    protected function authenticated(Request $req, $user)
+    public function authenticated(Request $req, $user)
     {
         AuditTrail::create([
             'user_id'   => $user->id,
@@ -71,5 +71,46 @@ class LoginController extends Controller
         $req->session()->invalidate();
 
         return $this->loggedOut($req) ?: redirect('/');
+    }
+
+    protected function credentials(Request $request)
+    {
+        $field = $this->field($request);
+
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->get('password'),
+        ];
+    }
+
+    /**
+     * Determine if the request field is email or username.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    public function field(Request $request)
+    {
+        $email = $this->username();
+
+        return filter_var($request->get($email), FILTER_VALIDATE_EMAIL) ? $email : 'username';
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $field = $this->field($request);
+
+        $messages = ["{$this->username()}.exists" => 'The account you are trying to login is not registered or it has been disabled.'];
+
+        $this->validate($request, [
+            $this->username() => "required|exists:users,{$field}",
+            'password' => 'required',
+        ], $messages);
     }
 }
