faa102ebe29b1b793be3005e2a6973e8cb520a5c
David Mendoza <davidmendozaofficial@gmail.com>
2019-08-29 18:33:59 +0800

    New Seeders
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -12,10 +12,29 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'Admin']);
-        Role::create(['name' => 'Cadet']);
-        Role::create(['name' => 'Encoder']);
-        Role::create(['name' => 'Applicant']);
-        Role::create(['name' => 'Principal']);
+        // Role::create(['name' => 'Admin']);
+        // Role::create(['name' => 'Cadet']);
+        // Role::create(['name' => 'Encoder']);
+        // Role::create(['name' => 'Applicant']);
+        // Role::create(['name' => 'Principal']);
+        // Role::create(['name' => 'Crewing Manager']);
+        // Role::create(['name' => 'Crewing Officer']);
+        // Role::create(['name' => 'Processing']);
+        $roles = [
+            'Admin',
+            'Cadet',
+            'Encoder',
+            'Applicant',
+            'Principal',
+            'Crewing Manager',
+            'Crewing Officer',
+            'Processing'
+        ];
+
+        foreach($roles as $role){
+            if(!Role::where("name", $role)->count()){
+                Role::create(["name" => $role]);
+            }
+        }
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -33,22 +33,22 @@ class UsersTableSeeder extends Seeder
 
         Ssap::create(["token" => strrev('123456')]);
 
-        User::create([
-        	'fname' => 'App',
-            'mname' => 'Lic',
-            'lname' => 'Cant',
-            'role' => 'Applicant',
-            'email' => 'applicant@applicant.com',
-            'birthday' => '1997-11-12',
-            'gender' => 'Male',
-            'address' => 'Manila, Phillipines',
-            'contact' => '09789456132',
-            'applicant' => true,
-            'email_verified_at' => now()->toDateTimeString(),
-            'password' => '123456'
-        ]);
+        // User::create([
+        // 	'fname' => 'App',
+        //     'mname' => 'Lic',
+        //     'lname' => 'Cant',
+        //     'role' => 'Applicant',
+        //     'email' => 'applicant@applicant.com',
+        //     'birthday' => '1997-11-12',
+        //     'gender' => 'Male',
+        //     'address' => 'Manila, Phillipines',
+        //     'contact' => '09789456132',
+        //     'applicant' => true,
+        //     'email_verified_at' => now()->toDateTimeString(),
+        //     'password' => '123456'
+        // ]);
 
-        Ssap::create(["token" => strrev('123456')]);
+        // Ssap::create(["token" => strrev('123456')]);
 
         $principals = [
             'SHINKO', 'KOSCO', 'TOEI', 'SMTECH', 'SC MARINE',
@@ -84,76 +84,76 @@ class UsersTableSeeder extends Seeder
             Ssap::create(["token" => strrev('123456')]);
         }
 
-        Applicant::create([
-            'user_id' => 2,
-            'birth_place' => 'Somewhere in Phillipines',
-            'religion' => 'Catholic',
-            'provincial_address' => 'Manila, Phillipines',
-            'provincial_contact' => '09123456789',
-            'age' => 20,
-            'height' => 5.5,
-            'weight' => 60,
-            'bmi' => 23,
-            'blood_type' => 'O',
-            'civil_status' => 'Single',
-            'tin' => 17238172937,
-            'sss' => 128736182736,
-            'waistline' => 5.9,
-            'shoe_size' => 8,
-            'clothes_size' => 'M',
-            'eye_color' => 'Black'
-        ]);
+        // Applicant::create([
+        //     'user_id' => 2,
+        //     'birth_place' => 'Somewhere in Phillipines',
+        //     'religion' => 'Catholic',
+        //     'provincial_address' => 'Manila, Phillipines',
+        //     'provincial_contact' => '09123456789',
+        //     'age' => 20,
+        //     'height' => 5.5,
+        //     'weight' => 60,
+        //     'bmi' => 23,
+        //     'blood_type' => 'O',
+        //     'civil_status' => 'Single',
+        //     'tin' => 17238172937,
+        //     'sss' => 128736182736,
+        //     'waistline' => 5.9,
+        //     'shoe_size' => 8,
+        //     'clothes_size' => 'M',
+        //     'eye_color' => 'Black'
+        // ]);
 
-        EducationalBackground::create([
-            'applicant_id' => 1,
-            'type' => 'Elementary',
-            'course' => null,
-            'year' => '2003-2009',
-            'school' => 'Some Elementary School',
-            'address' => 'Somewhere in Phillipines',
-        ]);
+        // EducationalBackground::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'Elementary',
+        //     'course' => null,
+        //     'year' => '2003-2009',
+        //     'school' => 'Some Elementary School',
+        //     'address' => 'Somewhere in Phillipines',
+        // ]);
 
-        EducationalBackground::create([
-            'applicant_id' => 1,
-            'type' => 'High School',
-            'course' => null,
-            'year' => '2009-2013',
-            'school' => 'Some High School',
-            'address' => 'Somewhere in Phillipines',
-        ]);
+        // EducationalBackground::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'High School',
+        //     'course' => null,
+        //     'year' => '2009-2013',
+        //     'school' => 'Some High School',
+        //     'address' => 'Somewhere in Phillipines',
+        // ]);
 
-        EducationalBackground::create([
-            'applicant_id' => 1,
-            'type' => 'College',
-            'course' => 'BSIT',
-            'year' => '2013-2019',
-            'school' => 'FEU Institute of Technology',
-            'address' => 'Sampaloc, Manila',
-        ]);
+        // EducationalBackground::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'College',
+        //     'course' => 'BSIT',
+        //     'year' => '2013-2019',
+        //     'school' => 'FEU Institute of Technology',
+        //     'address' => 'Sampaloc, Manila',
+        // ]);
 
-        FamilyData::create([
-            'applicant_id' => 1,
-            'type' => 'Father',
-            'fname' => 'Fa',
-            'mname' => 'Ther',
-            'lname' => 'Cant',
-            'age' =>  40,
-            'birthday' => '1997-11-12', 
-            'address' => 'Manila, Phillipines',
-            'occupation' => 'Seaman',
-        ]);
+        // FamilyData::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'Father',
+        //     'fname' => 'Fa',
+        //     'mname' => 'Ther',
+        //     'lname' => 'Cant',
+        //     'age' =>  40,
+        //     'birthday' => '1997-11-12', 
+        //     'address' => 'Manila, Phillipines',
+        //     'occupation' => 'Seaman',
+        // ]);
 
-        FamilyData::create([
-            'applicant_id' => 1,
-            'type' => 'Mother',
-            'fname' => 'Mo',
-            'mname' => 'Ther',
-            'lname' => 'Cant',
-            'age' =>  38,
-            'birthday' => '1997-11-12', 
-            'address' => 'Manila, Phillipines',
-            'occupation' => 'Housewife',
-        ]);
+        // FamilyData::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'Mother',
+        //     'fname' => 'Mo',
+        //     'mname' => 'Ther',
+        //     'lname' => 'Cant',
+        //     'age' =>  38,
+        //     'birthday' => '1997-11-12', 
+        //     'address' => 'Manila, Phillipines',
+        //     'occupation' => 'Housewife',
+        // ]);
 
         // FamilyData::create([
         //     'applicant_id' => 1,
@@ -164,36 +164,36 @@ class UsersTableSeeder extends Seeder
         //     'address' => 'Manila, Phillipines',
         // ]);
 
-        DocumentId::create([
-            'applicant_id' => 1,
-            'type' => 'PASSPORT',
-            'issuer' => 'DFA',
-            'number' => '123456',
-            'issue_date' => now()->startOfMonth(),
-            'expiry_date' => now()->endOfMonth(),
-        ]);
+        // DocumentId::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'PASSPORT',
+        //     'issuer' => 'DFA',
+        //     'number' => '123456',
+        //     'issue_date' => now()->startOfMonth(),
+        //     'expiry_date' => now()->endOfMonth(),
+        // ]);
 
-        DocumentId::create([
-            'applicant_id' => 1,
-            'type' => "SEAMAN'S BOOK",
-            'issuer' => 'MARINA',
-            'number' => '654321',
-            'issue_date' => now()->startOfMonth(),
-            'expiry_date' => now()->endOfMonth(),
-        ]);
+        // DocumentId::create([
+        //     'applicant_id' => 1,
+        //     'type' => "SEAMAN'S BOOK",
+        //     'issuer' => 'MARINA',
+        //     'number' => '654321',
+        //     'issue_date' => now()->startOfMonth(),
+        //     'expiry_date' => now()->endOfMonth(),
+        // ]);
 
-        DocumentId::create([
-            'applicant_id' => 1,
-            'type' => 'US-VISA',
-            'issuer' => 'US EMBASSY',
-            'number' => '756159',
-            'issue_date' => now()->startOfMonth(),
-            'expiry_date' => now()->endOfMonth(),
-        ]);
+        // DocumentId::create([
+        //     'applicant_id' => 1,
+        //     'type' => 'US-VISA',
+        //     'issuer' => 'US EMBASSY',
+        //     'number' => '756159',
+        //     'issue_date' => now()->startOfMonth(),
+        //     'expiry_date' => now()->endOfMonth(),
+        // ]);
 
-        ProcessedApplicant::create([
-            'applicant_id' => 1,
-            'status' => 'Vacation'
-        ]);
+        // ProcessedApplicant::create([
+        //     'applicant_id' => 1,
+        //     'status' => 'Vacation'
+        // ]);
     }
 }
