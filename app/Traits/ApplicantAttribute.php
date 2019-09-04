6755660a60d0163e3c5934c73521054ee40fe36f
David Mendoza <davidmendozaofficial@gmail.com>
2019-09-04 17:13:20 +0800

    Line up available only for admin and c.manager
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Traits/ApplicantAttribute.php
+++ app/Traits/ApplicantAttribute.php
@@ -8,14 +8,19 @@ trait ApplicantAttribute{
 		// 	        '<span class="fa fa-search" data-id="' . $this->id . '"></span>' .
 		// 	   '</a>&nbsp;' . 
 		$status = $this->status == "Lined-Up" ? $this->status : 'Line-Up';
-		return '<a class="btn btn-warning" data-toggle="tooltip" title="Export Application" data-id="' . $this->id . '">' .
+		$string = '<a class="btn btn-warning" data-toggle="tooltip" title="Export Application" data-id="' . $this->id . '">' .
 			        '<span class="fa fa-download" data-id="' . $this->id . '"></span>' .
 			   '</a>&nbsp;' .
 			   '<a class="btn btn-primary" data-toggle="tooltip" title="Edit Application" data-id="' . $this->id . '">' .
 			        '<span class="fa fa-pencil" data-id="' . $this->id . '"></span>' .
-			   '</a>&nbsp;' .
-			   '<a class="btn btn-info" data-toggle="tooltip" title="' . $status . '" data-id="' . $this->id . '">' .
-			        '<span class="fa fa-arrow-up" data-id="' . $this->id . '"></span>' .
 			   '</a>&nbsp;';
+
+		if(in_array(auth()->user()->role, ['Admin', 'Crewing Manager'])){
+			$string .= '<a class="btn btn-info" data-toggle="tooltip" title="' . $status . '" data-id="' . $this->id . '">' .
+					        '<span class="fa fa-arrow-up" data-id="' . $this->id . '"></span>' .
+					   '</a>&nbsp;';
+		}
+
+		return $string;
 	}
 }
