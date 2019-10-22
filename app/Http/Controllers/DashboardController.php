973239868dc26c13d129eea439b1b2450d0ab519
David Mendoza <davidmendozaofficial@gmail.com>
2019-10-22 20:38:40 +0800

    View some data in dashboard
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Models\{Applicant, LineUpContract};
+
 class DashboardController extends Controller
 {
     public function index(){
+    	$onBoard = LineUpContract::where('status', 'On Board')->get();
+    	$applicants = Applicant::select('id')->get();
+
     	return $this->_view('dashboard', [
-    		'title' => 'Dashboard'
+    		'title' 		=> 'Dashboard',
+    		'applicants'	=> $applicants,
+    		'onBoard' 		=> $onBoard,
     	]);
     }
 
