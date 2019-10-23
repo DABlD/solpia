176ea45f0e32154263923055d0878ccfc2b4cc97
David Mendoza <davidmendozaofficial@gmail.com>
2019-10-23 15:31:23 +0800

    DOB is Date of Birth
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/westernOnOff.blade.php
+++ resources/views/exports/westernOnOff.blade.php
@@ -20,7 +20,7 @@
 			<td>{{ $key + 1 }}</td>
 			<td>{{ $onSigner->abbr }}</td>
 			<td>{{ $onSigner->lname . ', ' . $onSigner->fname . ' ' . $onSigner->suffix . ' ' . $onSigner->mname }}</td>
-			<td>{{ now()->parse($onSigner->joining_date)->format('d-M-Y') }}</td>
+			<td>{{ now()->parse($onSigner->birthday)->format('d-M-Y') }}</td>
 			<td>{{ $onSigner->{'PASSPORTn'} ?? '-----' }}</td>
 			<td>{{ $onSigner->{"SEAMAN'S BOOKn"} ?? '-----' }}</td>
 			<td>{{ $onSigner->lastShip }}</td>
@@ -51,7 +51,7 @@
 			<td>{{ $key + 1 }}</td>
 			<td>{{ $onBoard->abbr }}</td>
 			<td>{{ $onBoard->lname . ', ' . $onBoard->fname . ' ' . $onBoard->suffix . ' ' . $onBoard->mname }}</td>
-			<td>{{ now()->parse($onBoard->joining_date)->format('d-M-Y') }}</td>
+			<td>{{ now()->parse($onBoard->birthday)->format('d-M-Y') }}</td>
 			<td>{{ $onBoard->{'PASSPORTn'} ?? '-----' }}</td>
 			<td>{{ $onBoard->{"SEAMAN'S BOOKn"} ?? '-----' }}</td>
 			<td></td>
