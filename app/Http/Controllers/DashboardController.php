76a6144bcf0a1331b7ebe2a735a0069470717390
David Mendoza <davidmendozaofficial@gmail.com>
2019-11-21 17:57:49 +0800

    Seperate dashboard data
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,11 +7,31 @@ use App\Http\Controllers\Controller;
 
 use App\Models\{Applicant, LineUpContract};
 
+use DB;
+
 class DashboardController extends Controller
 {
     public function index(){
-    	$onBoard = LineUpContract::where('status', 'On Board')->get();
-    	$applicants = Applicant::select('id')->get();
+        $condition = array();
+        if(auth()->user()->status == 2){
+            $condition = ['u.applicant', '=', 2];
+        }
+        elseif(auth()->user()->status > 0){
+            $condition = ['u.applicant', '>', 0];
+        }
+
+    	$onBoard = LineUpContract::join('applicants as a', 'a.id', '=', 'line_up_contracts.applicant_id')
+                                ->join('users as u', 'u.id', '=', 'a.user_id')
+                                ->where([
+                                    ['line_up_contracts.status', '=', 'On Board'], 
+                                    $condition
+                                    
+                                ])
+                                ->get();
+
+    	$applicants = Applicant::join('users as u', 'u.id', '=', 'applicants.user_id')
+                                ->where([$condition])
+                                ->get();
 
     	return $this->_view('dashboard', [
     		'title' 		=> 'Dashboard',
