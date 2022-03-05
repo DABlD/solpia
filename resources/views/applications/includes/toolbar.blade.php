575a1c3665200399dd6bb6f4862bb42311dd908c
David Mendoza <davidmendozaofficial@gmail.com>
2022-03-05 20:41:46 +0800

    word fix 2
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/applications/includes/toolbar.blade.php
+++ resources/views/applications/includes/toolbar.blade.php
@@ -2,7 +2,7 @@
 	<a href="{{ route('applications.index') }}" class="btn btn-info" data-toggle="tooltip" title="View All">
 		<span class="fa fa-list"></span>
 	</a>
-	<a href="{{ route('applications.create') }}" class="btn btn-primary" data-toggle="tooltip" title="Add Application">
+	<a href="{{ route('applications.create') }}" class="btn btn-primary" data-toggle="tooltip" title="Add New Crew">
 		<span class="fa fa-plus"></span>
 	</a>
 	<a href="{{ route('applications.export.all') }}" class="btn btn-warning" data-toggle="tooltip" title="Export All">
