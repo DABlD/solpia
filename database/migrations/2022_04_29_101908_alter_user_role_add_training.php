55c5d6db08552432629408260908e190746d7018
David Mendoza <davidmendozaofficial@gmail.com>
2022-04-29 10:20:14 +0800

    add training role
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ database/migrations/2022_04_29_101908_alter_user_role_add_training.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUserRoleAddTraining extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table){
+            DB::statement("ALTER TABLE users CHANGE COLUMN role role ENUM(
+                'Admin', 
+                'Cadet',
+                'Encoder',
+                'Applicant',
+                'Principal',
+                'Crewing Manager',
+                'Crewing Officer',
+                'Processing',
+                'Training'
+            ) NULL");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
