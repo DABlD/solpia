11d5a9d8686108115e7ff1edc7a7c3d45baae87c
David Mendoza <davidmendozaofficial@gmail.com>
2022-06-10 15:47:13 +0800

    ETO and ETR to ELECT only
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/Hmm.php
+++ app/Exports/Hmm.php
@@ -23,7 +23,7 @@ class Hmm implements WithMultipleSheets
         $sheets = [];
         array_push($sheets, new Hmm1($this->applicant, $this->type . '1'));
 
-        $ranks = ['C/O', '2/O', '3/O', 'BSN', 'AB', 'OS', 'AO', 'DCDT', '1AE', '2AE', '3AE', 'OLR1', 'OLR', 'ETO', 'ETR', 'A/E', 'CCK', '2CK'];
+        $ranks = ['C/O', '2/O', '3/O', 'BSN', 'AB', 'OS', 'AO', 'DCDT', '1AE', '2AE', '3AE', 'OLR1', 'OLR', 'ELECT', 'A/E', 'CCK', '2CK'];
         if(auth()->user()->fleet == "FLEET B" || auth()->user()->role == "Admin" && in_array($this->applicant->rank->abbr, $ranks)){
             $rank = $this->applicant->rank->abbr;
 
@@ -83,11 +83,7 @@ class Hmm implements WithMultipleSheets
                 $rows = 30;
                 $rank = "OS";
             }
-            elseif($rank == "ETO"){
-                $rows = 34;
-                $rank = "ELECT";
-            }
-            elseif($rank == "ETR"){
+            elseif($rank == "ELECT"){
                 $rows = 34;
                 $rank = "ELECT";
             }
