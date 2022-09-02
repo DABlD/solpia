6487bff0c05a2a200d378451d6f19fd3e64a4077
David Mendoza <davidmendozaofficial@gmail.com>
2022-09-02 14:36:25 +0800

    fix for last commit
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/PDFExport.php
+++ app/Exports/PDFExport.php
@@ -58,7 +58,7 @@ class PDFExport
             ['disembarkation_date', '=', null]
         ])
         ->select('applicant_id', 'rank_id', 'order')
-        ->join('ranks as r', 'r.id', '=', 'processed_applicants.rank_id')
+        ->join('ranks as r', 'r.id', '=', 'line_up_contracts.rank_id')
         ->get();
 
         foreach($applicants as $applicant){
