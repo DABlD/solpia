457aa7beef7f7f2623a0028ec6377f1ad34f31e3
David Mendoza <davidmendozaofficial@gmail.com>
2022-09-02 09:53:39 +0800

    Try fix 3
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/forms/y01_OnsignerDocs.blade.php
+++ resources/views/exports/forms/y01_OnsignerDocs.blade.php
@@ -1,9 +1,3 @@
-<style>
-	img{
-		image-orientation: none;
-	}
-</style>
-
 <table>
 	@php
 		foreach ($data as $key => $applicant) {
@@ -15,7 +9,7 @@
 		        	$img = public_path('files\\' . $applicant->applicant_id . '\\' . $doc[0]);
 		            echo "<tr>
 		            	<td>
-		            		<img src='$img' width='700px' height='1000px'>
+		            		<img src='$img' width='700px' height='1000px' style='image-orientation: none;'>
 		            	</td>
 		            </tr>";
 		        }
--- resources/views/exports/forms/y02_OffsignerDocs.blade.php
+++ resources/views/exports/forms/y02_OffsignerDocs.blade.php
@@ -10,7 +10,7 @@
 			        	$img = public_path('files\\' . $applicant->applicant_id . '\\' . $doc[0]);
 			            echo "<tr>
 			            	<td>
-			            		<img src='$img' width='700px' height='1000px'>
+			            		<img src='$img' width='700px' height='1000px' style='image-orientation: none;'>
 			            	</td>
 			            </tr>";
 			        }
