a8e595f23ecdcd3c2a4dfcab09b3744ba8bf5b85
David Mendoza <davidmendozaofficial@gmail.com>
2022-09-21 18:01:11 +0800

    fix font
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/Toei.php
+++ app/Exports/Toei.php
@@ -891,6 +891,9 @@ class Toei implements FromView, WithEvents, WithDrawings, WithColumnFormatting//
                 $rash3 += 1;
                 $event->sheet->getDelegate()->getStyle("E1:E100")->getFont()->setSize(8.5);
                 $event->sheet->getDelegate()->getStyle("E1:E130")->getFont()->setName('Times New Roman');
+                
+                $event->sheet->getDelegate()->getStyle("G22")->getFont()->setName('Times New Roman');
+                $event->sheet->getDelegate()->getStyle("G22")->getFont()->setSize(8.5);
 
                 // SET PRINT AREA
                 $event->sheet->getDelegate()->getPageSetup()->setPrintArea("A1:I$rash3");
