c27b04f2ca6071375168d63749f1a7f51e258836
David Mendoza <davidmendozaofficial@gmail.com>
2023-02-16 15:03:52 +0800

    Info Sheet
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/POEA/InfoSheet.php
+++ app/Exports/POEA/InfoSheet.php
@@ -249,7 +249,7 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
                 $event->sheet->getDelegate()->getPageMargins()->setHeader(0.5);
                 $event->sheet->getDelegate()->getPageMargins()->setFooter(0.5);
                 $event->sheet->getDelegate()->getPageSetup()->setHorizontalCentered(true);
-                $event->sheet->getDelegate()->getPageSetup()->setVerticalCentered(true);
+                // $event->sheet->getDelegate()->getPageSetup()->setVerticalCentered(true);
 
                 // SET PAGE BREAK PREVIEW
                 $temp = new \PhpOffice\PhpSpreadsheet\Worksheet\SheetView;
@@ -287,7 +287,8 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
 
                 // VT
                 $h[1] = [
-                    
+                    'A41', 'M41',
+                    'E40','B41'
                 ];
 
                 // HL B
@@ -302,6 +303,7 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
 
                 // HC VC
                 $h[4] = [
+                    'F8'
                 ];
 
                 // HL
@@ -310,6 +312,7 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
 
                 // B
                 $h[6] = [
+                    'A1:V150'
                 ];
 
                 // VC
@@ -317,10 +320,18 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
                 ];
 
                 $h['wrap'] = [
+                    'A40',
+                    'A41',
+                    'A45',
+                    'A46',
+                    'A47',
+                    'E40','B41'
                 ];
 
                 // SHRINK TO FIT
                 $h['stf'] = [
+                    'A1:V10', 'S16',
+                    'S21', 'P22'
                 ];
 
                 foreach($h as $key => $value) {
@@ -372,6 +383,9 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
 
                 // OUTSIDE BORDER THIN
                 $cells[3] = array_merge([
+                    'A1:E6',
+                    'R1:V10',
+                    'H18', 'J18', 'N18', 'P18', 'N19', 'P19'
                 ]);
 
                 // OUTSIDE BORDER MEDIUM
@@ -413,14 +427,49 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
 
                 // BBT
                 $cells[12] = array_merge([
+                    'C3:D3', 'D5',
+                    'T3:U3', 'T4:U4', 'U5', 'U6', 'U7', 'U8', 'U9',
+                    'D7:E7', 'B8:E8', 'B9:E9', 'C10:E10',
+                    'B13:F13', 'G13:L13', 'M13:R13', 'T13:V13',
+                    'G15:V15',
+                    'C16:G16', 'L16:O16', 'S16:V16',
+                    'C18:F18', 'S18:V18',
+                    'S19:V19',
+                    'C20:G20', 'O20:V20',
+                    'E21:L21', 'S21:V21',
+                    'P22:U22',
+                    'A23:V23',
+                    'A24:V24',
+                    'A25:V25',
+                    'A26:V26',
+                    'A27:V27',
+                    'A28:V28',
+                    'A30:V30',
+                    'A32:V32',
+                    'A33:V33',
+                    'A34:V34',
+                    'A35:V35',
+                    'A36:V36',
+                    'A37:V37',
+                    'E40:R40', 'T40:V40',
+                    'B41:L41', 'M41:R41', 'T41:V41',
+                    'E42:L42', 'N42:R42', 'T42:V42',
+                    'D43:K43', 'N43:V43',
+                    'C44:F44', 'I44:L44', 'N44:R44', 'T44:V44',
+                    'L45:R45',
+                    'L46:R46',
+                    'L47:V47',
+                    'N52:V52'
                 ]);
 
                 // LBT
                 $cells[13] = array_merge([
+                    'D18'
                 ]);
 
                 // RBT
                 $cells[14] = array_merge([
+                    'D18'
                 ]);
                 
                 foreach($cells as $key => $value){
@@ -433,17 +482,50 @@ class InfoSheet implements FromView, WithEvents//, WithDrawings//, ShouldAutoSiz
                 // $event->sheet->getDelegate()->getStyle('L46')->getFont()->setName('Marlett');
 
                 // COLUMN RESIZE
-                // $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(2);
+                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(9);
+                $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(7);
+                $event->sheet->getDelegate()->getColumnDimension('C')->setWidth(7);
+                $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(7);
+                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(2);
+                $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(7);
+                $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(6);
+                $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(4);
+                $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(8);
+                $event->sheet->getDelegate()->getColumnDimension('L')->setWidth(8);
+                $event->sheet->getDelegate()->getColumnDimension('M')->setWidth(8);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('O')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('P')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('Q')->setWidth(6);
+                $event->sheet->getDelegate()->getColumnDimension('R')->setWidth(5);
+                $event->sheet->getDelegate()->getColumnDimension('S')->setWidth(3);
+                $event->sheet->getDelegate()->getColumnDimension('T')->setWidth(9);
+                $event->sheet->getDelegate()->getColumnDimension('U')->setWidth(12);
+                $event->sheet->getDelegate()->getColumnDimension('V')->setWidth(2);
 
                 // ROW RESIZE
-                // $event->sheet->getDelegate()->getRowDimension(1)->setRowHeight(90);
+                for($i = 0; $i <= 56; $i++){
+                    $event->sheet->getDelegate()->getRowDimension($i)->setRowHeight(18);
+                }
+                $event->sheet->getDelegate()->getRowDimension(40)->setRowHeight(30);
+                $event->sheet->getDelegate()->getRowDimension(41)->setRowHeight(30);
+                $event->sheet->getDelegate()->getRowDimension(45)->setRowHeight(30);
+                $event->sheet->getDelegate()->getRowDimension(46)->setRowHeight(30);
+                $event->sheet->getDelegate()->getRowDimension(47)->setRowHeight(30);
                 
                 // SET PRINT AREA
                 // $event->sheet->getDelegate()->getPageSetup()->setPrintArea("C1:Y42");
+                
+                // SET FIT TO ONE PAGE
+                $event->sheet->getDelegate()->getPageSetup()->setFitToPage(TRUE);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToWidth(TRUE);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(TRUE);
 
                 // CUSTOM FONT AND STYLE TO DEFINED CELL
-                $event->sheet->getDelegate()->getStyle('A1:L150')->getFont()->setSize(10);
-                $event->sheet->getDelegate()->getStyle('A1:L150')->getFont()->setName('Arial');
+                $event->sheet->getDelegate()->getStyle('A1:V150')->getFont()->setSize(10);
+                $event->sheet->getDelegate()->getStyle('A1:V150')->getFont()->setName('Arial');
             },
         ];
     }
