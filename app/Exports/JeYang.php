f08b8e734b1aaa0ffba277a556e31e5f93ea3261
David Mendoza <davidmendozaofficial@gmail.com>
2023-03-15 18:05:42 +0800

    JE YANG BIODATA
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ app/Exports/JeYang.php
@@ -0,0 +1,510 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+// use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+use App\Models\Wage;
+
+class JeYang implements FromView, WithEvents, WithDrawings//, ShouldAutoSize
+{
+    public function __construct($data, $type){
+        $temp = Wage::where('rank_id', $data->pro_app->rank_id)
+                                    ->where('vessel_id', $data->pro_app->vessel_id)
+                                    ->first();
+
+        $data->wage = $temp ?? null;
+
+        $this->data     = $data;
+        $this->type     = $type;
+    }
+
+    public function view(): View
+    {
+        return view('exports.' . lcfirst($this->type), [
+            'data' => $this->data,
+        ]);
+    }
+
+    public function registerEvents(): array
+    {
+        $borderStyle = 
+        [
+            [//0
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ]
+            ],
+            [//1
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                    ],
+                ]
+            ],
+            [//2
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                ]
+            ],
+            [//3
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ]
+            ],
+            [//4
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                    ],
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                    ],
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                    ],
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                    ],
+                ]
+            ],
+            [//5
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                ]
+            ],
+            [//6
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => 'FFFFFF']
+                    ],
+                ]
+            ],
+            [//7
+                'borders' => [
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => 'FFFFFF']
+                    ],
+                ]
+            ],
+            [//8
+                'borders' => [
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => 'FFFFFF']
+                    ],
+                ]
+            ],
+            [//9
+                'borders' => [
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        'color' => ['argb' => 'FFFFFF']
+                    ],
+                ]
+            ],
+            [//10
+                'borders' => [
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    ],
+                ]
+            ],
+            [//11
+                'borders' => [
+                    'top' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                    ],
+                ]
+            ],
+            [//12
+                'borders' => [
+                    'bottom' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                    ],
+                ]
+            ],
+            [//13
+                'borders' => [
+                    'left' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                    ],
+                ]
+            ],
+            [//14
+                'borders' => [
+                    'right' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                    ],
+                ]
+            ],
+        ];
+
+        $fillStyle = [
+            [
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'color' => [
+                        'rgb' => 'FFFF00'
+                    ]
+                ],
+            ],
+            [
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'color' => [
+                        'rgb' => 'ebf8a4'
+                    ]
+                ],
+            ]
+        ];
+
+        $headingStyle = [
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP,
+                ]
+            ],
+            [
+                'font' => [
+                    'bold' => true
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ]
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+                ],
+            ],
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                ]
+            ],
+            [
+                'font' => [
+                    'bold' => true
+                ],
+            ],
+            [
+                'alignment' => [
+                    'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+                ],
+            ]
+        ];
+
+        $data = $this->data;
+
+        return [
+            AfterSheet::class => function(AfterSheet $event) use ($borderStyle, $fillStyle, $headingStyle, $data) {
+                // SHEET SETTINGS
+                $size = \PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4;
+                $event->sheet->getDelegate()->getPageSetup()->setPaperSize($size);
+                $event->sheet->getDelegate()->setTitle('TITLE', false);
+                $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(0);
+                $event->sheet->getDelegate()->getPageMargins()->setTop(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setBottom(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setRight(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setHeader(0.5);
+                $event->sheet->getDelegate()->getPageMargins()->setFooter(0.5);
+                $event->sheet->getDelegate()->getPageSetup()->setHorizontalCentered(true);
+                $event->sheet->getDelegate()->getPageSetup()->setVerticalCentered(true);
+
+                // SET PAGE BREAK PREVIEW
+                $temp = new \PhpOffice\PhpSpreadsheet\Worksheet\SheetView;
+                $event->sheet->getParent()->getActiveSheet()->setSheetView($temp->setView('pageBreakPreview'));
+
+                // CELL COLOR
+                // $event->sheet->getDelegate()->getStyle('E3:E7')->getFont()->getColor()->setRGB('0000FF');
+
+                // TEXT ROTATION
+                // $event->sheet->getDelegate()->getStyle('B11')->getAlignment()->setTextRotation(90);
+
+                // FUNCTIONS
+                // $osSize = sizeof($this->linedUps);
+                // $ofsSize = sizeof($this->onBoards);
+
+                // GET AFTER ONSIGNERS
+                // $ar = function($c1, $r1, $c2 = null, $r2 = null, $ofs = false) use($osSize, $ofsSize){
+                //     $size = $osSize;
+                //     $temp = $c1 . ($r1 + $size);
+                //     if($c2 != null){
+                //         $temp .= ':' . $c2 . ($r2 + ($size + ($ofs ? $ofsSize : 0)));
+                //     }
+
+                //     return $temp;
+                // };
+
+                // FONT SIZES
+
+                // HEADINGS
+
+                // HC B
+                $h[0] = [
+                    
+                ];
+
+                // VT
+                $h[1] = [
+                    
+                ];
+
+                // HL B
+                $h[2] = [
+                    
+                ];
+
+                // HC
+                $h[3] = [
+                    
+                ];
+
+                // HC VC
+                $h[4] = [
+                    'A3:O3'
+                ];
+
+                // HL
+                $h[5] = [
+                ];
+
+                // B
+                $h[6] = [
+                ];
+
+                // VC
+                $h[7] = [
+                ];
+
+                $h['wrap'] = [
+                    'A3:O3'
+                ];
+
+                // SHRINK TO FIT
+                $h['stf'] = [
+                    'K4', 'G4:G' . (4 + sizeof($data->sea_service)),
+                    'C4'
+                ];
+
+                foreach($h as $key => $value) {
+                    foreach($value as $col){
+                        if($key === 'wrap'){
+                            $event->sheet->getDelegate()->getStyle($col)->getAlignment()->setWrapText(true);
+                        }
+                        elseif($key == 'stf'){
+                            $event->sheet->getDelegate()->getStyle($col)->getAlignment()->setWrapText(false);
+                            $event->sheet->getDelegate()->getStyle($col)->applyFromArray([
+                                'alignment' => [
+                                    'shrinkToFit' => true
+                                ],
+                            ]);
+                        }
+                        else{
+                            $event->sheet->getDelegate()->getStyle($col)->applyFromArray($headingStyle[$key]);
+                        }
+                    }
+                }
+
+                // FILLS
+                $fills[0] = [
+                    'A2:O2'
+                ];
+
+                $fills[1] = [
+                    
+                ];
+
+                foreach($fills as $key => $value){
+                    foreach($value as $cell){
+                        $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($fillStyle[$key]);
+                    }
+                }
+
+                // BORDERS
+
+                // ALL BORDER THIN
+                $cells[0] = array_merge([
+                    'A2:O3',
+                ]);
+
+                // ALL BORDER MEDIUM
+                $cells[1] = array_merge([
+                ]);
+
+                // ALL BORDER THICK
+                $cells[2] = array_merge([
+                ]);
+
+                // OUTSIDE BORDER THIN
+                $cells[3] = array_merge([
+                    'A4:A' . (4 + sizeof($data->sea_service)),
+                    'B4:B' . (4 + sizeof($data->sea_service)),
+                    'C4:C' . (4 + sizeof($data->sea_service)),
+                    'D4:D' . (4 + sizeof($data->sea_service)),
+                    'E4:E' . (4 + sizeof($data->sea_service)),
+                    'F4:F' . (4 + sizeof($data->sea_service)),
+                    'G4:G' . (4 + sizeof($data->sea_service)),
+                    'H4:H' . (4 + sizeof($data->sea_service)),
+                    'I4:I' . (4 + sizeof($data->sea_service)),
+                    'J4:J' . (4 + sizeof($data->sea_service)),
+                    'K4:K' . (4 + sizeof($data->sea_service)),
+                    'L4:L' . (4 + sizeof($data->sea_service)),
+                    'M4:M' . (4 + sizeof($data->sea_service)),
+                    'N4:N' . (4 + sizeof($data->sea_service)),
+                    'O4:O' . (4 + sizeof($data->sea_service))
+                ]);
+
+                // OUTSIDE BORDER MEDIUM
+                $cells[4] = array_merge([
+                ]);
+
+                // OUTSIDE BORDER THICK
+                $cells[5] = array_merge([
+                ]);
+
+                // TOP REMOVE BORDER
+                $cells[6] = array_merge([
+                ]);
+
+                // BRB
+                $cells[7] = array_merge([
+                ]);
+
+                // LRB
+                $cells[8] = array_merge([
+
+                ]);
+
+                // RRB
+                $cells[9] = array_merge([
+                ]);
+
+                // TRB
+                $cells[10] = array_merge([
+                ]);
+
+                // TBT - TOP BORDER THIN
+                $cells[10] = array_merge([
+                ]);
+
+                // TBT - TOP BORDER THIN
+                $cells[11] = array_merge([
+                ]);
+
+                // BBT
+                $cells[12] = array_merge([
+                ]);
+
+                // LBT
+                $cells[13] = array_merge([
+                ]);
+
+                // RBT
+                $cells[14] = array_merge([
+                ]);
+                
+                foreach($cells as $key => $value){
+                    foreach($value as $cell){
+                        $event->sheet->getDelegate()->getStyle($cell)->applyFromArray($borderStyle[$key]);
+                    }
+                }
+
+                // FOR THE CHECK
+                // $event->sheet->getDelegate()->getStyle('L46')->getFont()->setName('Marlett');
+
+                // COLUMN RESIZE
+                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(5);
+                $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(11);
+                $event->sheet->getDelegate()->getColumnDimension('C')->setWidth(22);
+                $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(11);
+                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(42);
+                $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(14);
+                $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(40);
+                $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(11);
+                $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(11);
+                $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(17);
+                $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(22);
+                $event->sheet->getDelegate()->getColumnDimension('L')->setWidth(9);
+                $event->sheet->getDelegate()->getColumnDimension('M')->setWidth(9);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(9);
+
+                // ROW RESIZE
+                // $event->sheet->getDelegate()->getRowDimension(1)->setRowHeight(90);
+                
+                // SET PRINT AREA
+                // $event->sheet->getDelegate()->getPageSetup()->setPrintArea("C1:Y42");
+
+                // CUSTOM FONT AND STYLE TO DEFINED CELL
+                // $event->sheet->getDelegate()->getStyle('A1:L150')->getFont()->setSize(14);
+                // $event->sheet->getDelegate()->getStyle('A1:L150')->getFont()->setName('Arial');
+            },
+        ];
+    }
+
+    public function drawings()
+    {
+        $drawing2 = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+        $drawing2->setName('Avatar');
+        $drawing2->setDescription('Avatar');
+        $drawing2->setPath(public_path($this->data->user->avatar));
+        $drawing2->setResizeProportional(false);
+        $drawing2->setHeight(75);
+        $drawing2->setWidth(74);
+        $drawing2->setOffsetX(2);
+        $drawing2->setOffsetY(2);
+        $drawing2->setCoordinates('B4');
+
+        return [$drawing2];
+    }
+}
