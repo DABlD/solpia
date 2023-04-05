0b9bf6542d5f7c9da6c36a384e9c377e5a3d0452
David Mendoza <davidmendozaofficial@gmail.com>
2023-04-05 15:33:55 +0800

    update rank importname
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Imports/AddVesselWageScale.php
+++ app/Imports/AddVesselWageScale.php
@@ -10,54 +10,67 @@ class AddVesselWageScale implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
-        $temp = Vessel::where('status', 'active')->get();
-        $vessels = [];
-
-        foreach($temp as $vessel){
-            $name = str_replace("/", "", $vessel->name);
-            $vessels[$name] = $vessel;
+        $ranks = Rank::all();
+        foreach($ranks as $rank){
+            $rank->importName = $rank->name;
+            $rank->save();
         }
 
-        // dd($rows->first());
-        $names = $rows->unique('position')->pluck('position')->toArray();
-        $ranks = Rank::all()->pluck('name')->toArray();
         
-        $match = [];
-        $nomatch = [];
 
-        foreach($names as $name){
-            $name = trim($name);
+        // $temp = Vessel::where('status', 'active')->get();
+        // $vessels = [];
 
-            if($name == "TRAINEE ELECTRICIAN" || $name == "ELECTRICIAN TRAINEE"){
-                $name = "TRAINEE ELECTRICIAN";
-            }
-            elseif($name == "TRAINEE REEFERMAN" || $name == "REEFERMAN TRAINEE" || $name == "TRAINEE REFFERMAN"){
-                $name = "TRAINEE REEFERMAN";
-            }
-            elseif($name == "REEFERMAN" || $name == "REFFERMAN"){
-                $name = "REEFERMAN";
-            }
-            elseif($name == "DECK BOY" || $name == "DECKBOY"){
-                $name = "DECK BOY";
-            }
-            elseif($name == "DECK HAND" || $name == "DECKHAND"){
-                $name = "DECK HAND";
-            }
-            elseif($name == "ELECTRICIAN" || $name == "ELECTRCIAN"){
-                $name = "ELECTRICIAN";
-            }
+        // foreach($temp as $vessel){
+        //     $name = str_replace("/", "", $vessel->name);
+        //     $vessels[$name] = $vessel;
+        // }
 
-            if(in_array($name, $ranks)){
-                array_push($match, $name);
-            }
-            else{
-                array_push($nomatch, $name);
-            }
-        }
+        // $names = $rows->unique('vessel')->pluck('vessel')->toArray();
+        // $temp2 = array_keys($vessels);
 
-        dd('match', $match, 'no match', $nomatch, '----', $ranks);
+        // $match = [];
+        // $nomatch = [];
+
+        // foreach($temp2 as $name){
+        //     if(in_array($name, $names)){
+        //         array_push($match, $name);
+        //     }
+        //     else{
+        //         array_push($nomatch, $name);
+        //     }
+        // }
+
+        // dd('match', $match, 'nomatch', $nomatch, $temp2);
+        // $ranks = Rank::all()->groupBy('name');
+
+        // foreach($rows as $row){
+        // }
     }
 }
+
+// foreach($names as $name){
+//     $name = trim($name);
+
+//     if($name == "TRAINEE ELECTRICIAN" || $name == "ELECTRICIAN TRAINEE"){
+//         $name = "TRAINEE ELECTRICIAN";
+//     }
+//     elseif($name == "TRAINEE REEFERMAN" || $name == "REEFERMAN TRAINEE" || $name == "TRAINEE REFFERMAN"){
+//         $name = "TRAINEE REEFERMAN";
+//     }
+//     elseif($name == "REEFERMAN" || $name == "REFFERMAN"){
+//         $name = "REEFERMAN";
+//     }
+//     elseif($name == "DECK BOY" || $name == "DECKBOY"){
+//         $name = "DECK BOY";
+//     }
+//     elseif($name == "DECK HAND" || $name == "DECKHAND"){
+//         $name = "DECK HAND";
+//     }
+//     elseif($name == "ELECTRICIAN" || $name == "ELECTRCIAN"){
+//         $name = "ELECTRICIAN";
+//     }
+// }
 
 // TRIM ALL RANKS FIRST
 //TRAINEE ELECTRICIAN / ELECTRICIAN TRAINEE
