1a347f7d9da3c9c7a8ecd7143c1dbf33d0ab8fde
David Mendoza <davidmendozaofficial@gmail.com>
2023-05-09 09:30:25 +0800

    change title
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/Reports/Prospect.php
+++ app/Exports/Reports/Prospect.php
@@ -245,7 +245,7 @@ class Prospect implements FromView, WithEvents//, WithDrawings//, ShouldAutoSize
                 // SHEET SETTINGS
                 $size = \PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4;
                 $event->sheet->getDelegate()->getPageSetup()->setPaperSize($size);
-                $event->sheet->getDelegate()->setTitle('TITLE', false);
+                $event->sheet->getDelegate()->setTitle('WEEKLY REPORT', false);
                 $event->sheet->getDelegate()->getPageSetup()->setFitToHeight(0);
                 $event->sheet->getDelegate()->getPageMargins()->setTop(0.5);
                 $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
