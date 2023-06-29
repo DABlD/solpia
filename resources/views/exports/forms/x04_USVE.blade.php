20299e440a60baa30d4978aad804d718fd6b9bf9
David Mendoza <davidmendozaofficial@gmail.com>
2023-06-29 16:22:04 +0800

    revised USVE
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/forms/x04_USVE.blade.php
+++ resources/views/exports/forms/x04_USVE.blade.php
@@ -1,362 +1,269 @@
 @php
-	$center = "text-align: center;";
-	$middle = "vertical-align: middle;";
-	$bold = "font-weight: bold;";
-	$und = "text-decoration: underline;";
+	$b = "font-weight: bold;";
+	$c = "text-align: center;";
+	$bc = "$b$c";
 	$blue = "color: #0000FF;";
+	$u = "text-decoration: underline;";
+
+	// dd($data);
 @endphp
 
 <table>
 	<tr>
-		<td colspan="10" style="{{ $center }} {{ $bold }} {{ $und }} font-size: 14px;">
+		<td colspan="12" style="height: 50px;"></td>
+	</tr>
+
+	<tr>
+		<td colspan="12" style="{{ $bc }} {{ $u }}">
 			US VISA ENDORSEMENT FORM
 		</td>
 	</tr>
 
 	<tr>
 		<td colspan="8"></td>
-		<td style="text-align: left; {{ $bold }}">DATE/TIME</td>
-		<td style="text-align: right;">{{ now()->format('d-F-Y') }}</td>
+		<td colspan="2" style="{{ $b }} text-align: right; height: 30px;">DATE / TIME:</td>
+		<td style="{{ $c }} {{ $u }}">{{ now()->format("d-M-Y") }}</td>
 	</tr>
 
 	<tr>
-		<td colspan="10"></td>
+		<td colspan="4" style="{{ $b }}">TO</td>
+		<td colspan="2">:</td>
+		<td colspan="2" style="{{ $bc }}">Processing Section - Visa</td>
+		<td colspan="4"></td>
 	</tr>
 
 	<tr>
-		<td style="font-size: 10px; {{ $bold }}" colspan="3">
-			TO:
-		</td>
-		<td colspan="5" style="{{ $bold }}">
-			Processing Section - Visa
-		</td>
-		<td colspan="2"></td>
+		<td colspan="4" style="{{ $b }}">NAME</td>
+		<td colspan="2">:</td>
+		<td colspan="2" style="{{ $blue }}">{{ $data->user->namefull }}</td>
+		<td colspan="1" style="{{ $b }}">RANK:</td>
+		<td colspan="2" style="{{ $c }}">{{ $data->rank }}</td>
+	</tr>
+
+	@php
+		$lastVessel = null;
+		if($data->sea_service->count()){
+			$data->sea_service = $data->sea_service->sortByDesc('sign_off');
+			$lastVessel = $data->sea_service->first();
+		}
+	@endphp
+
+	<tr>
+		<td colspan="4" style="{{ $b }}">LAST VESSEL</td>
+		<td colspan="2">:</td>
+		<td colspan="2" style="{{ $blue }}">{{ $lastVessel ? $lastVessel->vessel_name : "-" }}</td>
+		<td colspan="2" style="{{ $b }}">S/OFF DATE:</td>
+		<td colspan="1" style="{{ $c }}">{{ $lastVessel ? $lastVessel->sign_off->format('d-M-Y') : "-" }}</td>
 	</tr>
 
 	<tr>
-		<td style="font-size: 10px; {{ $bold }}" colspan="3">
-			NAME:
-		</td>
-		<td colspan="4" style="font-size: 10px; {{ $bold }} {{ $blue }}">
-			{{ $data->user->lname }}, {{ $data->user->fname }} {{ $data->user->mname }}
-		</td>
-		<td></td>
-		<td style="font-size: 10px; {{ $bold }}">
-			RANK:
-		</td>
-		<td style="font-size: 10px; {{ $bold }} {{ $center }} {{ $blue }}">
-			{{ $data->rank }}
-		</td>
+		<td colspan="4" style="{{ $b }}">Prospect Vessel</td>
+		<td colspan="2">:</td>
+		<td colspan="2" style="{{ $blue }}">{{ $data->pro_app->vessel->vessel_name }}</td>
+		<td colspan="2" style="{{ $b }}">S/ON DATE:</td>
+		<td colspan="1" style="{{ $c }}">{{ isset($data->data['eld']) ? now()->parse($data->data['eld'])->format('d-M-Y') : "-" }}</td>
 	</tr>
 
 	<tr>
-		<td style="font-size: 10px; {{ $bold }}" colspan="3">
-			LAST VESSEL:
-		</td>
-		<td colspan="4" style="font-size: 10px; {{ $bold }} {{ $blue }}">
-			{{ $data->sea_service[0]->vessel_name }}
-		</td>
-		<td></td>
-		<td style="font-size: 10px; {{ $bold }}">
-			S/OFF DATE:
-		</td>
-		<td style="font-size: 10px; {{ $bold }} {{ $center }} {{ $blue }}">
-			{{ $data->sea_service[0]->sign_off ? $data->sea_service[0]->sign_off->format('d-F-y') : '---' }}
-		</td>
-	</tr>
-
-	<tr>
-		<td style="font-size: 10px; {{ $bold }}" colspan="3">
-			PROSPECT VESSEL:
-		</td>
-		<td colspan="4" style="font-size: 10px; {{ $bold }} {{ $blue }}">
-			{{ $data->vessel ? $data->vessel->name : "---" }}
-		</td>
-		<td></td>
-		<td style="font-size: 10px; {{ $bold }}">
-			S/ON DATE:
-		</td>
-		<td style="font-size: 10px; {{ $bold }} {{ $center }} {{ $blue }}">
-			{{ $data->departure ? $data->departure->format('d-F-y') : '---' }}
+		<td colspan="12" style="{{ $bc }} {{ $u }} height: 25px;">
+			Remarks: (Must clearly explain to concerned seafarer)
 		</td>
 	</tr>
 
 	<tr>
-		<td style="{{ $middle }} {{ $center }} {{ $bold }} {{ $und }} height: 30px;" colspan="10">
-			Remarks: (Must clearly explain to concert seafarer)
-		</td>
-	</tr>
-
-	<tr>
-		<td style="{{ $bold }} {{ $und }}" colspan="10">
-			AA. FOR VISA PREPARATION ONLY (No vessel assignment yet)
-		</td>
+		<td colspan="12" style="{{ $b }} {{ $u }}">AA. FOR VISA PREPARATION ONLY (No vessel assignment yet)</td>
 	</tr>
 
 	<tr>
 		<td></td>
-		<td style="{{ $bold }} {{ $und }}" colspan="9">
-			• SEAFARERS FUNDED APPLICATION
-		</td>
+		<td style="{{ $bc }}">&#8226;</td>
+		<td colspan="10" style="{{ $b }} {{ $u }}">SEAFARERS FUNDED APPLICATION</td>
 	</tr>
 
 	<tr>
-		<td></td>
-		<td colspan="9">
-			1. That applicable fee such as Visa application / processing cost + processing employment
+		<td rowspan="2"></td>
+		<td rowspan="2">1.‎</td>
+		<td rowspan="2" colspan="10">
+			That applicable fee such as Visa application / processing cost + processing employment contract cost shall be on seafarer's account.
 		</td>
 	</tr>
 
+	<tr></tr>
+
+	<tr>
+		<td rowspan="2"></td>
+		<td rowspan="2">2.‎</td>
+		<td rowspan="2" colspan="10">
+			That such refusal or denial, on seafarer's account expenses, I fully understand that I cannot reimburse visa application / processing cost.
+		</td>
+	</tr>
+
+	<tr></tr>
+
+	<tr>
+		<td rowspan="2"></td>
+		<td rowspan="2">3.‎</td>
+		<td rowspan="2" colspan="10">
+			That I understand US visa will be processed under company's name &#38; responsibility and furthermore its advantages of having valid US visa, this I fully agreed that I can only withdraw documents only
+		</td>
+	</tr>
+
+	<tr></tr>
 	<tr>
 		<td colspan="2"></td>
-		<td colspan="8">
-			contract cost shall be on seafarers account.
+		<td style="{{ $c }}">after</td>
+		<td colspan="3" style="{{ $b }} {{ $u }}">
+			THREE (3) months
 		</td>
+		<td colspan="6">after payment of all accrued and related expenses.</td>
+	</tr>
+
+	<tr>
+		<td rowspan="3"></td>
+		<td rowspan="3">4.‎</td>
+		<td rowspan="3" colspan="10" style="{{ $blue }} height: 19px;">
+			That I understand &#38; fully agree that refund or reimbursement will only be paid / deposited to my account after embarkation date within 2-3 months or upon recovery from principal. And I am aware that refund is applicable only for vesssel with US Visa requiremnent.
+		</td>
+	</tr>
+
+	<tr></tr>
+	<tr></tr>
+
+	<tr>
+		<td colspan="12"></td>
 	</tr>
 
 	<tr>
 		<td></td>
-		<td colspan="9">
-			2. That such refusal or denial, on seafarer’s account expenses, I fully understand that I cannot
-		</td>
+		<td style="{{ $bc }}">&#8226;</td>
+		<td colspan="10" style="{{ $b }} {{ $u }}">COMPANY FUNDED APPLICATION</td>
 	</tr>
 
 	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			reimburse visa application / processing cost.
+		<td rowspan="3"></td>
+		<td rowspan="3">5.‎</td>
+		<td rowspan="3" colspan="10">
+			That visa application / processing cost in case of visa denial or refusal by the US Embassy, I am fully aware to repay the entire expenses incurred for the visa in favor of Solpia Marine &#38; Ship Management Inc.
 		</td>
+	</tr>
+
+	<tr></tr>
+	<tr></tr>
+
+	<tr>
+		<td></td>
+		<td>6.‎</td>
+		<td colspan="2" style="{{ $bc }}">NO REFUND</td>
+		<td colspan="8">for the cases of embarking non-US Visa required vessels</td>
 	</tr>
 
 	<tr>
 		<td></td>
-		<td colspan="9">
-			3. That I understand USA visa will be processed under company’s name &#38; responsibility and
+		<td>7.‎</td>
+		<td colspan="7" style="{{ $b }}">
+			NO OTHER FEES TO BE COLLECTED for the NON-APPEARANCE
 		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			furthermore its advantages of having valid USA visa, thus I fully agreed that I can only withdraw
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			documents only after <b><u>THREE (3) months</u></b> and after payment of all accrued and related
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			expenses.
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			4. That I understand &#38; fully agree that refund or reimbursement will only be paid / deposited to my
-		</td>
+		<td colspan="3">asides from the visa fees paid to</td>
 	</tr>
 
 	<tr>
 		<td colspan="2"></td>
-		<td colspan="8">
-			account after embarkation date within 2-3 months or upon recovery from principal. And I am
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			aware that refund is applicable only for vessel with US Visa requirement.
-		</td>
+		<td colspan="10">the bank as per Machine Readable Visa (MRV) application fee based on current rate.</td>
 	</tr>
 
 	<tr>
 		<td></td>
-		<td colspan="9" style="{{ $bold }}">
-			• COMPANY FUNDED APPLICATION
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			5. That visa application / processing cost, in case of visa denial or refusal by the US Embassy, I
+		<td>8.‎</td>
+		<td colspan="10" style="{{ $b }}">
+			Both for REGULAR and SEAVRP applications an LBC delivery fee of PHP 440
 		</td>
 	</tr>
 
 	<tr>
 		<td colspan="2"></td>
-		<td colspan="8">
-			fully aware to repay the entire expenses incurred for the visa in favor of Solpia Marine &#38; Ship
+		<td colspan="10">to be paid by company and will be a salary deduction once crew embarked.</td>
+	</tr>
+
+	<tr>
+		<td rowspan="3"></td>
+		<td rowspan="3">9.‎</td>
+		<td rowspan="3" colspan="10" style="{{ $blue }} height: 19px;">
+			That I am fully aware that visa receipts is valid only for 1 YEAR from the date of payment. If visa appointment was CANCELLED due to early embarkation, will re-sched the visa application upon disembarkation IF RECEIPT IS STILL VALID.
 		</td>
 	</tr>
 
+	<tr></tr>
+	<tr></tr>
+
 	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			Management Inc.
-		</td>
+		<td colspan="12"></td>
 	</tr>
 
 	<tr>
-		<td></td>
-		<td colspan="9">
-			6. NO REFUND for the cases of embarking non-USA visa required vessels
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			7. <b>NO OTHER FEES TO BE COLLECTED</b> for the <b>NON-APPERANCE</b> asides from the visa fees 
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			paid to the bank as per (Machine Readable Visa (MRV) application fee based on current rate
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			8. <b>Both for REGULAR and SEAVRP applications a 2GO document delivery fee of PHP 435</b> to be
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			paid by company and will be a salary deduction once crew embarked.
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			9. That I am fully aware <b>that the visa receipt is valid only for 1 year</b> from the date of payment.
-		</td>
-	</tr>
-
-	<tr>
-		<td></td>
-		<td colspan="9">
-			10. That I understand if my visa appointment will be CANCELLED due to early embarkation, the company
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			will <b>reschedule my application upon disembarkation if RECEIPT IS STILL VALID. If receipt</b>
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"></td>
-		<td colspan="8">
-			<b>already expired, I fully agree to pay a new visa fee.</b>
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="10"></td>
-	</tr>
-
-	<tr>
-		<td colspan="10" style="{{ $bold }} {{ $und }}">
+		<td colspan="12" style="{{ $b }} {{ $u }}">
 			BB. FOR LINED UP CREW
 		</td>
 	</tr>
 
 	<tr>
-		<td colspan="10" style="height: 65px; vertical-align: middle;">
-			In the event that cancellations of deployment, as advice by owners/principal, with this company looking forward that crew understands such changes and give Solpia ample time to re-assign seafarer to other vessel. However, if crews opt to withdraw documents with US Visa crew, must repay the remaining validity (pro rata basis)/ unused portion. Owners / principal paid for the US Visa expecting that the validity of said Visa will be used on-board their vessel.
+		<td colspan="12"></td>
+	</tr>
+
+	<tr>
+		<td rowspan="5" colspan="12">
+				&#32;‎ ‎ ‎ ‎ ‎ In the event that cancellations of deployment, as advice by owners/principal, with this company looking forward that crew understands such changes and give Solpia ample time to re-assign seafarer to other vessel. However, if crews opt to withdraw documents with US Visa, crew must repay the remaining validity (pro rated basis) / unused portion. Owners / principal paid for the US Visa expecting that the validity of said US Visa will be used on-board their vessel.
 		</td>
 	</tr>
 
-	<tr>
-		<td></td>
-	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr></tr>
+	<tr></tr>
 
 	<tr>
-		<td colspan="10"></td>
-	</tr>
-
-	<tr>
-		<td colspan="3">Passport exp:</td>
-		<td colspan="2" style="{{ $center }} {{ $blue }}">
+		<td colspan="3" style="{{ $c }} height: 17px;">Passport exp :</td>
+		<td colspan="3" style="{{ $c }}">
 			{{ isset($data->document_id->PASSPORT) ? $data->document_id->PASSPORT->expiry_date ? $data->document_id->PASSPORT->expiry_date->format('d-M-y') : "N/A" : "N/A" }}
 		</td>
-		<td>SIRB exp:</td>
-		<td style="{{ $center }} {{ $blue }}">
+		<td style="{{ $c }}">SIRB exp :</td>
+		<td style="{{ $c }}">
 			{{ isset($data->document_id->{"SEAMAN'S BOOK"}) ? $data->document_id->{"SEAMAN'S BOOK"}->expiry_date ? $data->document_id->{"SEAMAN'S BOOK"}->expiry_date->format('d-M-y') : "N/A" : "N/A" }}
 		</td>
-		<td>Crewing Officer:</td>
-		<td colspan="2" style="{{ $center }}">
-			{{ auth()->user()->gender == "Male" ? "Mr" : "Ms" }}. {{ auth()->user()->fname }} {{ auth()->user()->lname }}
-		</td>
-	</tr>
-
-	<tr>
-		<td style="color: #FF0000; {{ $bold }} font-style: italic;" colspan="3">
-			@php
-				$bool = false;
-				foreach($data->sea_service as $ss){
-					if(str_contains($ss->manning_agent, 'SOLPIA')){
-						$bool = true;
-					}
-				}
-			@endphp
-
-			@if($bool)
-				EX-CREW
+		<td colspan="4">
+			Crewing Officer: 
+			@if(auth()->user()->gender == "Male")
+				MR. 
 			@else
-				NEW HIRE
+				MS. 
 			@endif
+			
+			{{ auth()->user()->fname }} {{ auth()->user()->lname }}
 		</td>
 	</tr>
 
 	<tr>
-		<td colspan="3" style="{{ $bold }} {{ $center }}">
-			Conforme:
-		</td>
-		<td colspan="4"></td>
-		<td colspan="2" style="{{ $bold }}">
-			Endorsed by:
-		</td>
+		<td colspan="7" style="{{ $bc }}">Conformed by:</td>
+		<td colspan="2"></td>
+		<td colspan="2" style="{{ $bc }}">Endorsed by:</td>
 		<td></td>
 	</tr>
 
 	<tr>
-		<td colspan="10"></td>
-	</tr>
-
-	<tr>
-		<td style="vertical-align: bottom; {{ $center }}" colspan="6">
-			{{ $data->user->lname }}, {{ $data->user->fname }} {{ $data->user->suffix }} {{ $data->user->mname }}
-		</td>
-		<td></td>
-		<td colspan="3" style="{{ $center }}">
+		<td colspan="7" style="{{ $bc }}">{{ $data->user->namefull }}</td>
+		<td colspan="2"></td>
+		<td colspan="2" style="{{ $bc }}">
 			@if(auth()->user()->fleet == "FLEET A")
-				Ms. Precian Cervantes
+		    	Ms. Precian Cervantes
 			@elseif(auth()->user()->fleet == "FLEET B")
 				Mr. Adulf Kit Jumawan
 			@elseif(auth()->user()->fleet == "FLEET C")
-				Ms. Jeannette Solidum
+				Ms. Jeanette Solidum
 			@elseif(auth()->user()->fleet == "FLEET D")
 				Ms. Thea Guerra
 			@elseif(auth()->user()->fleet == "FLEET E")
 				Mr. Dennis Quiño
+			@elseif(auth()->user()->fleet == "FISHING")
+				Mr. Ricardo Amparo
 			@elseif(auth()->user()->fleet == "TOEI")
 				Mr. Neil Romano
 			@endif
@@ -364,44 +271,42 @@
 	</tr>
 
 	<tr>
-		<td style="vertical-align: bottom; {{ $center }}" colspan="6">
-			Seafarer Signature over printed name
-		</td>
+		<td colspan="7" style="{{ $bc }}">Seafarer Signature over printed name</td>
+		<td colspan="2"></td>
+		<td colspan="2" style="{{ $bc }}">Crewing Manager</td>
 		<td></td>
-		<td colspan="3" style="{{ $center }}">
-			Crewing Manager
-		</td>
 	</tr>
 
 	<tr>
-		<td style="border: solid 1px #000000; {{ $bold }} {{ $center }}">
-			{{ $data->data['chargeTo'] ? "X" : "" }}
-		</td>
-		<td style="font-size: 9px;" colspan="3">Charge to Seafarer</td>
-		<td style="border: solid 1px #000000; {{ $bold }} {{ $center }}">
-			{{ $data->data['chargeTo'] ? "" : "X" }}
-		</td>
-		<td style="font-size: 9px;" colspan="2">Charge to SMI</td>
-		<td colspan="3">Noted by:</td>
+		<td colspan="12"></td>
 	</tr>
 
 	<tr>
-		<td colspan="10"></td>
+		<td colspan="9"></td>
+		<td colspan="2" style="{{ $c }}">Noted by:</td>
+		<td></td>
 	</tr>
 
 	<tr>
-		<td colspan="7"></td>
-		<td style="{{ $center }}" colspan="3">C/E. ROMANO A. MARIANO</td>
+		<td colspan="9"></td>
+		<td colspan="2" style="{{ $c }}"></td>
+		<td></td>
+	</tr>
+
+	<tr>
+		<td colspan="9"></td>
+		<td colspan="2" style="{{ $c }}">CE Romano A. Mariano</td>
+		<td></td>
 	</tr>
 
 	<tr>
 		<td></td>
-		<td style="color: #C1C1C1;" colspan="3">SMOP-USVEF-12</td>
-		<td colspan="3"></td>
-		<td colspan="3" style="{{ $center }}">PRESIDENT</td>
-	</tr>
-
-	<tr>
-		<td style="font-size: 7px; text-align: right; vertical-align: bottom;" colspan="10">8,000.00</td>
+		<td style="{{ $c }}">{{ $data->data['chargeTo'] ? "X" : "" }}</td>
+		<td colspan="3" style="{{ $c }}">Charge to Seafarer</td>
+		<td style="{{ $c }}">{{ $data->data['chargeTo'] ? "" : "X" }}</td>
+		<td>Charge to SMI</td>
+		<td colspan="2"></td>
+		<td colspan="2" style="{{ $bc }}">President</td>
+		<td></td>
 	</tr>
 </table>
