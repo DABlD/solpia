ed69a3903e34c3b1b1de36bf82166a25d0c23a8a
David Mendoza <davidmendozaofficial@gmail.com>
2023-11-08 14:10:50 +0800

    fleet b new checklist
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -1,17 +1,7 @@
 function getChecklist(fleet){
 	if(fleet == "FLEET B"){
 		return `
-			<optgroup label="HMM"></optgroup>
-				<option value="hmm_con_kor">&nbsp;&nbsp;&nbsp;HMM - CONTAINER - KOREAN FLAG</option>
-				<option value="hmm_con_mal">&nbsp;&nbsp;&nbsp;HMM - CONTAINER - MALTA FLAG</option>
-				<option value="hmm_con_mar">&nbsp;&nbsp;&nbsp;HMM - CONTAINER - MARSHALL FLAG</option>
-				<option value="hmm_con_pan">&nbsp;&nbsp;&nbsp;HMM - CONTAINER - PANAMA FLAG</option>
-			<optgroup label="KOSCO"></optgroup>
-				<option value="kos_bul_mar">&nbsp;&nbsp;&nbsp;KOSCO - BULK - MARSHALL</option>
-				<option value="kos_bul_lib">&nbsp;&nbsp;&nbsp;KOSCO - BULK - LIBERIA</option>
-				<option value="kos_cb_lib">&nbsp;&nbsp;&nbsp;KOSCO - CONTAINER & BULK - PANAMA</option>
-			<optgroup label="POSSM"></optgroup>
-				<option value="possm">&nbsp;&nbsp;&nbsp;Default</option>
+			<option value="template"> 		DEFAULT 		 			</option>
 		`;
 	}
 	else if(fleet == "FLEET C"){
