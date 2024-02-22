a2f3a21c6242a574e8074a04ddb073a59c2def87
David Mendoza <davidmendozaofficial@gmail.com>
2024-02-22 10:25:41 +0800

    day to days
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/seaServiceCertificate.blade.php
+++ resources/views/exports/seaServiceCertificate.blade.php
@@ -95,7 +95,7 @@
 			<td style="border: 1px solid black; font-size: 9px; height: 17px;">{{ now()->format('d-M-y') }}</td>
 			<td style="border: 1px solid black; font-size: 9px; height: 17px;">{{ $ob->rank->abbr }}</td>
 			<td style="border: 1px solid black; font-size: 9px; height: 17px;">
-				{{ '=IF(DATEDIF(G' . $start . ',H' . $start . ',"y")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"y")&" yr ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"ym")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"ym")&" mos ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"md")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"md")&" day")' }}
+				{{ '=IF(DATEDIF(G' . $start . ',H' . $start . ',"y")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"y")&" yr ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"ym")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"ym")&" mos ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"md")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"md")&" days")' }}
 			</td>
 		</tr>
 	@endif
@@ -124,7 +124,7 @@
 				<td style="border: 1px solid black; font-size: 9px; height: 17px;">{{ $ss->sign_off ? $ss->sign_off->format('d-M-y') : "-" }}</td>
 				<td style="border: 1px solid black; font-size: 9px; height: 17px;">{{ $ss->rank }}</td>
 				<td style="border: 1px solid black; font-size: 9px; height: 17px;">
-					{{ '=IF(DATEDIF(G' . $start . ',H' . $start . ',"y")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"y")&" yr ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"ym")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"ym")&" mos ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"md")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"md")&" day")' }}
+					{{ '=IF(DATEDIF(G' . $start . ',H' . $start . ',"y")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"y")&" yr ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"ym")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"ym")&" mos ")&IF(DATEDIF(G' . $start . ',H' . $start . ',"md")=0,"",DATEDIF(G' . $start . ',H' . $start . ',"md")&" days")' }}
 				</td>
 			</tr>
 		@endif
