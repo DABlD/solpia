06be3dd00dfac7e12515544c9f4dc78fac7c552b
David Mendoza <davidmendozaofficial@gmail.com>
2024-04-15 18:04:12 +0800

    update format
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/requestToProcess.blade.php
+++ resources/views/exports/requestToProcess.blade.php
@@ -92,7 +92,7 @@
 			<td colspan="3" style="{{ $bc }}">RANK</td>
 			<td colspan="2" style="{{ $bc }}">FLAG</td>
 			<td colspan="4" style="{{ $bc }}">VESSEL</td>
-			<td colspan="2" style="{{ $bc }}">PORT / COUNTRY</td>
+			<td colspan="2" style="{{ $bc }}">FLAG CERT.</td>
 			<td style="{{ $bc }}">DEPARTURE</td>
 		</tr>
 	@else
@@ -118,13 +118,13 @@
 				<td colspan="2" style="{{ $center }} height: 13px;">{{ $crew->rank ? $data->flag : "" }}</td>
 				<td colspan="4" style="{{ $center }} height: 13px;">{{ $crew->vessel }}</td>
 				<td colspan="2" style="{{ $center }} height: 13px;">
-					@if(isset($crew->port))
+					{{-- @if(isset($crew->port))
 						{{ $crew->port }}
 					@else
 						@if($crew->rank)
 							{{ $data->port }}
 						@endif
-					@endif
+					@endif --}}
 				</td>
 				<td style="{{ $center }} height: 13px;">
 					@if($crew->rank != "")
