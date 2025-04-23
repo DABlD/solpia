6794c9fcc4ff86d80e75f6bcb02819def98ff0f8
David Mendoza <davidmendozaofficial@gmail.com>
2025-04-23 17:00:01 +0800

    fleet b onboard crew for ms lhen request
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -1841,7 +1841,7 @@ class ApplicationsController extends Controller
 
         foreach($users as $user){
             if(isset($user->crew->pro_app) && $user->crew->pro_app->status == "On Board" && $user->crew->pro_app->vessel->principal_id == 256){
-                echo $user->namefull . ';' . $user->crew->pro_app->rank->abbr . ';' . $user->birthday . ';' . $user->crew->pro_app->updated_at . '<br>';
+                echo $user->lname . ';' . $user->fname . ';' . $user->mname . ';' . $user->crew->civil_status . ';' . $user->crew->pro_app->rank->abbr . ';' . $user->birthday . ';' . $user->crew->pro_app->updated_at . '<br>';
             }
         }
     }
