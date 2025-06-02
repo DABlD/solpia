e1831220c921de6a7948c886cb5a2c01f8840b19
David Mendoza <davidmendozaofficial@gmail.com>
2025-06-02 18:53:51 +0800

    fix pres name
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/kosco2.blade.php
+++ resources/views/exports/kosco2.blade.php
@@ -518,7 +518,8 @@
 		<td></td>
 		<td></td>
 		<td colspan="2">Signature:</td>
-		<td colspan="5">ERASQUIN, SHIRLEY</td>
+		{{-- <td colspan="5">ERASQUIN, SHIRLEY</td> --}}
+		<td colspan="5">CE ROMANO MARIANO</td>
 		<td></td>
 	</tr>
 	<tr><td colspan="15"></td></tr>
