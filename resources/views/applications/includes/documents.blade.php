7e400dfdfe9e44af02212505bf5a2695ffb9c330
David Mendoza <davidmendozaofficial@gmail.com>
2025-10-01 09:58:13 +0800

    add schengen visa
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/applications/includes/documents.blade.php
+++ resources/views/applications/includes/documents.blade.php
@@ -134,7 +134,7 @@
         	var id_options = "";
         	var idOptions = [
         		'', 'PASSPORT', 'US-VISA', "SEAMAN BOOK", 'MCV', 'JAPANESE VISA', "SEAMANS REGISTRATION CERTIFICATE", "SID",
-        		'KOREAN VISA', 'CHINESE VISA', 'New Zealand eTA', 'HRIS ID'
+        		'KOREAN VISA', 'CHINESE VISA', 'New Zealand eTA', 'HRIS ID', 'SCHENGEN VISA'
         	];
 
         	idOptions.forEach(docu => {
