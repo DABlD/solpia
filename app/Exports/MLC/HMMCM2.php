404deea04f8ac5a12f4462b1ed7d940ac6ea07e9
David Mendoza <davidmendozaofficial@gmail.com>
2026-01-20 16:45:24 +0800

    HMM Oriental Greenstone MLC
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/MLC/HMMCM2.php
+++ app/Exports/MLC/HMMCM2.php
@@ -23,7 +23,8 @@ class HMMCM2 implements FromView, WithEvents, WithDrawings//, ShouldAutoSize
             // ,"M/V ATLANTIC AFFINITY","M/V OCEAN FLORA","M/V PACIFIC CHAMP",
             // "M/V KRISTIAN OLDENDORFF","M/V ATLANTIC BONANZA",
             "M/V HYUNDAI PARAMOUNT",                                                                                                    // FLEET B PART OF 1ST BATCH 
-            "M/T ORIENTAL AQUAMARINE", "M/T UNIVERSAL CHALLENGER", "M/T UNIVERSAL FRONTIER", "M/T UNIVERSAL INNOVATOR",                 // FLEET C PART OF 1ST BATCH
+            "M/T ORIENTAL AQUAMARINE", "M/T UNIVERSAL CHALLENGER", "M/T UNIVERSAL FRONTIER", "M/T UNIVERSAL INNOVATOR",
+            "M/T ORIENTAL GREENSTONE"                                                                                                   // FLEET C PART OF 1ST BATCH
 
             "M/V HMM HARMONY","M/V HMM MASTER","M/V HMM MIRACLE",                                                                       // FLEET B PART OF 2ND BATCH
 
