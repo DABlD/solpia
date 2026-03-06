1ff34cf3d1465efd2cd9f030c694b7f987448189
David Mendoza <davidmendozaofficial@gmail.com>
2026-03-06 17:15:50 +0800

    fix yf no expiry
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/forms/x06_Ext_Prom_Form.blade.php
+++ resources/views/exports/forms/x06_Ext_Prom_Form.blade.php
@@ -140,7 +140,8 @@
 		<td style="{{ $center }} height: 40px;">
 			{{ $yf->number ?? '-' }}
 			<br style='mso-data-placement:same-cell;' />
-			{{ $yf ? $yf->expiry_date->format('d-M-Y') : '-' }}
+			{{-- {{ $yf ? $yf->expiry_date->format('d-M-Y') : '-' }} --}}
+			{{ "-" }}
 		</td>
 		<td style="{{ $center }} height: 40px;">
 			{{ $mcv->number ?? '-' }}
