9ede4857e1cb81c0029ede97c3bdafee35477b47
David Mendoza <davidmendozaofficial@gmail.com>
2026-04-14 15:49:16 +0800

    display remark on prospect report
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/reports/prospect2.blade.php
+++ resources/views/exports/reports/prospect2.blade.php
@@ -18,10 +18,11 @@
 		<td style="{{ $bc }}">Name</td>
 		<td style="{{ $bc }}">Age</td>
 		<td style="{{ $bc }}">Contact</td>
-		<td style="{{ $bc }}">Email</td>
+		{{-- <td style="{{ $bc }}">Email</td> --}}
 		<td style="{{ $bc }}">USV</td>
 		<td style="{{ $bc }}">Exp</td>
 		<td style="{{ $bc }}">Date</td>
+		<td style="{{ $bc }}">Remark</td>
 		<td style="{{ $bc }}">TOTAL</td>
 		<td style="{{ $bc }} color: #FF0000;">=SUBTOTAL(3, A3:A{{ sizeof($data) + 3 }})</td>
 	</tr>
@@ -34,11 +35,12 @@
 			<td style="{{ $c }}">{{ $prospect['name'] }}</td>
 			<td style="{{ $c }}">{{ isset($prospect['birthday']) ? now()->parse($prospect['birthday'])->age : $prospect['age'] ?? '-' }}</td>
 			<td style="{{ $c }}">{{ $prospect['contact'] }}</td>
-			<td style="{{ $c }}">{{ $prospect['email'] }}</td>
+			{{-- <td style="{{ $c }}">{{ $prospect['email'] }}</td> --}}
 			<td style="{{ $c }}">{{ isset($prospect['usv']) ? now()->parse($prospect['usv'])->format('F j, Y') : '-' }}</td>
 			<td style="{{ $c }}">{{ $prospect['exp'] }}</td>
 			{{-- <td style="{{ $c }}">{{ implode(',', json_decode($prospect['exp'])) }}</td> --}}
 			<td style="{{ $c }}">{{ now()->parse($prospect['created_at'])->format('F j, Y') }}</td>
+			<td style="{{ $c }}">{{ $prospect['remarks'] }}</td>
 		</tr>
 	@endforeach
 </table>
