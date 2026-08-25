2a2d68824f266994870d4b49152da2289be029fa
David Mendoza <davidmendozaofficial@gmail.com>
2026-08-25 21:50:06 +0800

    fix image height
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/forms/medicalReferral_WINDSOR.blade.php
+++ resources/views/exports/forms/medicalReferral_WINDSOR.blade.php
@@ -463,10 +463,10 @@ INSTRUCTIONS TO APPLICANT
 
 
 @if($ppImg)
-<img src="{{ public_path("files/$data->id/$ppImg") }}" width="700px" height="500px">
+<img src="{{ public_path("files/$data->id/$ppImg") }}" width="700px" height="650px">
 <br>
 @endif
 
 @if($sbImg)
-<img src="{{ public_path("files/$data->id/$ppImg") }}" width="700px" height="500px">
+<img src="{{ public_path("files/$data->id/$ppImg") }}" width="700px" height="650px">
 @endif
