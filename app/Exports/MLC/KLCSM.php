4aaef66a6ee6b8fcaf459d54e8f2758b0fcd570f
David Mendoza <davidmendozaofficial@gmail.com>
2026-09-23 23:43:46 +0800

    sm falcon mlc address change
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Exports/MLC/KLCSM.php
+++ app/Exports/MLC/KLCSM.php
@@ -32,10 +32,17 @@ class KLCSM implements FromView, WithEvents, WithDrawings//, ShouldAutoSize
             "M/V CK BLUEBELL" => "JJR-111067",
         ];
 
-        if(in_array($applicant->vessel->name, ['M/T SM NAVIGATOR', 'M/T SM FALCON', 'M/T SM OSPREY'])){
+        if(in_array($applicant->vessel->name, ['M/T SM NAVIGATOR', 'M/T SM OSPREY'])){
             $shipowner = "KOREA LINE CORPORATION";
             $phoneNumber = "+82-2-3701-0114";
             $address = "3F 30, Sinchonnyeok-ro, Seodaemun-gu, Seoul, Korea";
+            $employer = "MIN SANG GI";
+            $identification = "101-81-24624";
+        }
+        elseif(in_array($applicant->vessel->name, ['M/T SM FALCON'])){
+            $shipowner = "KOREA LINE CORPORATION";
+            $phoneNumber = "+82-2-3701-0114";
+            $address = "30, Sinchonnyeok-ro, Seodaemun-gu, Seoul, Republic of Korea";
             $employer = "MIN SANG GI";
             $identification = "101-81-24624";
         }
